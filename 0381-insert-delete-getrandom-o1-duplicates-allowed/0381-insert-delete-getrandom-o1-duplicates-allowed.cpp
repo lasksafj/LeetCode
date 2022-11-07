cc1b8aa3d0d19eb405d21c3b75a615d67cb33266
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2022-11-06 18:21:30 -0800

    Time: 624 ms (24.28%), Space: 107.8 MB (44.56%) - LeetHub

--- 0381-insert-delete-getrandom-o1-duplicates-allowed/0381-insert-delete-getrandom-o1-duplicates-allowed.cpp
+++ 0381-insert-delete-getrandom-o1-duplicates-allowed/0381-insert-delete-getrandom-o1-duplicates-allowed.cpp
@@ -1,32 +1,33 @@
 class RandomizedCollection {
 public:
-    unordered_map<int,vector<int>> m;
-    vector<pair<int,int>> v;
+    unordered_map<int,unordered_set<int>> m;
+    vector<int> v;
     RandomizedCollection() {
         
     }
     
     bool insert(int val) {
         bool res = m.find(val) == m.end() || m[val].size() == 0;
-        m[val].push_back(v.size());
-        v.push_back({val, m[val].size()-1});
+        m[val].insert(v.size());
+        v.push_back(val);
         return res;
     }
     
     bool remove(int val) {
         if (m[val].size() > 0) {
-            int p = m[val].back();
-            m[v.back().first][v.back().second] = p;
-            swap(v[p], v.back());
+            int p = *m[val].begin();
+            m[val].erase(p);
+            v[p] = v.back();
+            m[v[p]].insert(p);
+            m[v[p]].erase(v.size()-1);
             v.pop_back();
-            m[val].pop_back();
             return true;
         }
         return false;
     }
     
     int getRandom() {
-        return v[rand() % v.size()].first;
+        return v[rand() % v.size()];
     }
 };
 
