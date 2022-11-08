f23363c33190ddc80ccef6d7a2461badc83111c9
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2022-11-08 13:16:44 -0800

    Time: 512 ms (53.08%), Space: 14.9 MB (59.64%) - LeetHub

--- 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.py
+++ 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.py
@@ -3,10 +3,9 @@ class Solution:
         occur_pos = {}
         for c in string.ascii_lowercase:
             occur_pos[c] = -1
-        prev = [0] * len(s)
-        dp = [0] * len(s)
+        prev, dp = 0,0
         for i in range(len(s)):
-            prev[i] = (prev[i-1] if i>0 else 0) + (i - occur_pos[s[i]])
-            dp[i] = (dp[i-1] if i>0 else 0) + prev[i]
+            prev += i - occur_pos[s[i]]
+            dp += prev
             occur_pos[s[i]] = i
-        return dp[len(s)-1]
+        return dp
