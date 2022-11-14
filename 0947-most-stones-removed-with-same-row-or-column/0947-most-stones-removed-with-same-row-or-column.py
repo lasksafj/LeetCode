4c6565c8ea3be2d4dceb09c3b11f2492261accb6
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2022-11-13 20:58:03 -0800

    Time: 2589 ms (24.56%), Space: 15.3 MB (34.06%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.py
@@ -0,0 +1,31 @@
+class Solution:
+    def removeStones(self, stones: List[List[int]]) -> int:
+        n = len(stones)
+        adj = [[] for _ in range(n)]
+        deg = [0] * n
+        for i in range(n):
+            for j in range(i+1, n):
+                if stones[i][0] == stones[j][0] or stones[i][1] == stones[j][1]:
+                    adj[i].append(j)
+                    adj[j].append(i)
+                    deg[i] += 1
+                    deg[j] += 1
+        vis = [0] * n
+        def dfs(i):
+            if vis[i]:
+                return 0
+            vis[i] = 1
+            res = 1
+            for ne in adj[i]:
+                res += dfs(ne)
+            return res
+        
+        res = 0
+        for i in range(n):
+            a = 0
+            if adj[i] != [] and vis[i] == 0:
+                a = -1
+                a += dfs(i)
+            res += a
+        return res
+                    
