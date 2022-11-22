bf7bc1a5335d81a9cc56957806378d02c1a51114
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2022-11-21 19:57:20 -0800

    Time: 2160 ms (66.33%), Space: 343.8 MB (6.48%) - LeetHub

---
+++ 2478-number-of-beautiful-partitions/2478-number-of-beautiful-partitions.py
@@ -0,0 +1,17 @@
+class Solution:
+    def beautifulPartitions(self, s: str, k: int, minLength: int) -> int:
+        n = len(s)
+        @cache
+        def sol(i, k):
+            if k == 0 and i <= n:
+                return 1
+            if i >= n:
+                return 0
+            res = sol(i+1, k)
+            if s[i] in '2357' and s[i-1] not in '2357':
+                res = (res + sol(i+minLength, k-1)) % 1000000007
+            return res % 1000000007
+        if s[0] not in '2357' or s[-1] in '2357':
+            return 0
+        return sol(minLength, k-1) % 1000000007
+            
