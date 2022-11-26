b982727cf82fbed82cf009c9e3cd70f96fb2e61a
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2022-11-26 14:14:50 -0800

    Time: 1677 ms (16.67%), Space: 22.7 MB (16.67%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.py
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.py
@@ -1,22 +1,18 @@
 class Solution:
     def bestClosingTime(self, customers: str) -> int:
         n = len(customers)
-        left = [[0]*2 for _ in range(n)]
-        right = [[0]*2 for _ in range(n)]
-        left[0][0] = 0+(customers[0] == 'N')
-        left[0][1] = 0+(customers[0] == 'Y')
+        left = [0]*n
+        right = [0]*n
+        left[0] = 0+(customers[0] == 'N')
         for i in range(1,n):
-            left[i][0] = left[i-1][0] + (customers[i] == 'N')
-            left[i][1] = left[i-1][1] + (customers[i] == 'Y')
-        right[-1][0] = 0+(customers[-1] == 'N')
-        right[-1][1] = 0+(customers[-1] == 'Y')
+            left[i] = left[i-1] + (customers[i] == 'N')
+        right[-1] = 0+(customers[-1] == 'Y')
         for i in range(n-2,-1,-1):
-            right[i][0] = right[i+1][0] + (customers[i] == 'N')
-            right[i][1] = right[i+1][1] + (customers[i] == 'Y')
+            right[i] = right[i+1] + (customers[i] == 'Y')
         minpen = inf
         res = 0
         for i in range(n+1):
-            cur = (left[i-1][0] if i>0 else 0) + (right[i][1] if i<n else 0)
+            cur = (left[i-1] if i>0 else 0) + (right[i] if i<n else 0)
             if minpen > cur:
                 minpen = cur
                 res = i
