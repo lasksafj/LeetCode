d69c80c866cffed1b005ea8b03081a152a3b82e9
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2022-11-27 09:57:10 -0800

    Time: 492 ms (99.41%), Space: 52.3 MB (91.72%) - LeetHub

--- 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.py
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.py
@@ -1,19 +1,23 @@
 class Solution:
     def numberOfArithmeticSlices(self, nums: List[int]) -> int:
         n = len(nums)
-        dp = [[defaultdict(int) for _ in range(3)] for _ in range(n)]
+        dp = [{} for _ in range(n)]
         res = 0
         for i in range(n):
             for j in range(i):
                 diff = nums[i] - nums[j]
-                for k in range(3):
-                    if k < 2:
-                        dp[i][k+1][diff] += dp[j][k][diff] + (1 if k==0 else 0)
+                a = 0
+                if diff not in dp[j]:
+                    if diff in dp[i]:
+                        dp[i][diff] += 1
                     else:
-                        dp[i][k][diff] += dp[j][k][diff]
-                # print(dp[i][2][diff])
-                # res += dp[i][2][diff]
-            # print(dp[i][2])
-            for e,v in dp[i][2].items():
-                res += v
+                        dp[i][diff] = 1
+                else:
+                    a = dp[j][diff]
+                    if diff in dp[i]:
+                        dp[i][diff] += dp[j][diff] + 1
+                    else:
+                        dp[i][diff] = dp[j][diff] + 1
+                    
+                res += a
         return res
