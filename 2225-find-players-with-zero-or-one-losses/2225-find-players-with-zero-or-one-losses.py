a7b49de20a770c2b9512bcd167a60711ff44450a
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2022-11-27 17:54:11 -0800

    Time: 1750 ms (99.93%), Space: 68.7 MB (77.41%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
@@ -1,20 +1,23 @@
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
-        losses_count = [-1] * 100001
-
+        zero_loss = set()
+        one_loss = set()
+        more_losses = set()
+        
         for winner, loser in matches:
-            if losses_count[winner] == -1:
-                losses_count[winner] = 0
-            if losses_count[loser] == -1:
-                losses_count[loser] = 1
+            # Add winner
+            if (winner not in one_loss) and (winner not in more_losses):
+                zero_loss.add(winner)
+            # Add or move loser.
+            if loser in zero_loss:
+                zero_loss.remove(loser)
+                one_loss.add(loser)
+            elif loser in one_loss:
+                one_loss.remove(loser)
+                more_losses.add(loser)
+            elif loser in more_losses:
+                continue
             else:
-                losses_count[loser] += 1
+                one_loss.add(loser)          
             
-        answer = [[], []]
-        for i in range(100001):
-            if losses_count[i] == 0:
-                answer[0].append(i)
-            elif losses_count[i] == 1:
-                answer[1].append(i)
-                
-        return answer
+        return [sorted(list(zero_loss)), sorted(list(one_loss))]
