28cde09b35a0a72fbc7f50223330018208cc54c0
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2022-12-03 17:49:45 -0800

    Time: 1137 ms (84.41%), Space: 24.9 MB (56.92%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.py
+++ 2256-minimum-average-difference/2256-minimum-average-difference.py
@@ -1,16 +1,14 @@
 class Solution:
     def minimumAverageDifference(self, nums: List[int]) -> int:
         n = len(nums)
-        post = [0]*n
-        post[-1] = nums[-1]
-        for i in range(n-2,-1,-1):
-            post[i] = post[i+1] + nums[i]
+        s = sum(nums)
         left = 0
         res = 0
         avgdif = inf
         for i in range(n):
             left += nums[i]
-            a = abs(left//(i+1) - ((post[i+1]//(n-i-1)) if i+1<n else 0))
+            s -= nums[i]
+            a = abs(left//(i+1) - ((s//(n-i-1)) if i+1<n else 0))
             if avgdif > a:
                 avgdif = a
                 res = i
