239a130ed2c965d1645647088a791a7883b8b665
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2022-12-20 20:55:52 -0800

    Time: 1963 ms (26.82%), Space: 22 MB (35.09%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.py
+++ 0886-possible-bipartition/0886-possible-bipartition.py
@@ -5,23 +5,19 @@ class Solution:
             adj[a].append(b)
             adj[b].append(a)
         vis = [0]*(n+1)
-        vis[0] = 2
         
-        def dfs(prev, cur, color):
-            if vis[cur] > 0:
-                return vis[cur] == color
-            vis[cur] = 3 - vis[prev]
-            if vis[cur] != color:
-                return False
-            res = True
+        def dfs(cur, color):
+            vis[cur] = color
             for ne in adj[cur]:
-                res &= dfs(cur, ne, 3 - vis[cur])
-            return res
+                if vis[ne] == color:
+                    return False
+                elif vis[ne] == 0 and not dfs(ne, 3 - vis[cur]):
+                    return False
+            return True
         
-        res = True
         for i in range(1,n+1):
-            if vis[i] == 0:
-                res &= dfs(0, i, 1)
-        return res
+            if vis[i] == 0 and not dfs(i, 1):
+                return False
+        return True
             
         
