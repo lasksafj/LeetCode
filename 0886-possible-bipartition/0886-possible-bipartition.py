c101d794555d53ee696ce6dccac6c7c6a7b5f337
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2022-12-23 15:38:02 -0800

    Time: 858 ms (68.55%), Space: 20 MB (74.85%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.py
+++ 0886-possible-bipartition/0886-possible-bipartition.py
@@ -6,6 +6,7 @@ class Solution:
             adj[b].append(a)
         
         parent = list(range(n+1))
+        rank = [0]*(n+1)
         def root(x):
             if parent[x] == x:
                 return x
@@ -14,8 +15,14 @@ class Solution:
         def union(x,y):
             xr,yr = root(x),root(y)
             if xr == yr:
-                return False
-            parent[yr] = xr
+                return
+            if rank[xr] < rank[yr]:
+                parent[xr] = yr
+            elif rank[xr] > rank[yr]:
+                parent[yr] = xr
+            else:
+                parent[yr] = xr
+                rank[xr] += 1
         
         
         for i in range(1,n+1):
