85f3bfa022b9e95a7ae8473cbf35c214d08dbfd4
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2022-12-26 14:04:37 -0800

    Time: 863 ms (54.71%), Space: 63.1 MB (35.19%) - LeetHub

--- 2518-number-of-great-partitions/2518-number-of-great-partitions.py
+++ 2518-number-of-great-partitions/2518-number-of-great-partitions.py
@@ -1,20 +1,15 @@
 class Solution:
     def countPartitions(self, nums: List[int], k: int) -> int:
-        n = len(nums)
-#         dp = [[0]*k for _ in range(n)]
-        
-#         for i in range(n):
-#             dp[i][0] = 1
-#             for j in range(1, k):
-#                 dp[i][j] = dp[i-1][j] + (dp[i-1][j-nums[i]] if j >= nums[i] else 0)
-#         print(dp)
-        if sum(nums) < k*2:
+        if sum(nums) < 2*k:
             return 0
-            
-        dp = [0] * k
-        dp[0] = 1
-        for i in range(n):
-            for j in range(k-1, nums[i]-1, -1):
-                dp[j] = dp[j] + dp[j-nums[i]]
-        # print(dp)
-        return (2**n - 2*sum(dp)) % 1000000007
+        n = len(nums)
+        dp = [[0]*k for _ in range(n)]
+        dp[0][0] = 1
+        for j in range(1, k):
+            dp[0][j] = (1 if nums[0] == j else 0)
+        for i in range(1, n):
+            dp[i][0] = 1
+            for j in range(1, k):
+                dp[i][j] = dp[i-1][j] + (dp[i-1][j-nums[i]] if j >= nums[i] else 0)
+
+        return (2**n - 2*sum(dp[-1])) % 1000000007
