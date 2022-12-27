2ba5a3a5cb077fe9e70ca2e4b9839dde37ef8555
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2022-12-26 20:28:21 -0800

    Time: 1066 ms (72.92%), Space: 28.8 MB (21.88%) - LeetHub

---
+++ 2306-naming-a-company/2306-naming-a-company.py
@@ -0,0 +1,10 @@
+class Solution:
+    def distinctNames(self, ideas: List[str]) -> int:
+        m = defaultdict(set)
+        for w in ideas:
+            m[w[0]].add(w[1:])
+        res = 0
+        for a,seta in m.items():
+            for b,setb in m.items():
+                res += len(seta - setb) * len(setb - seta)
+        return res
