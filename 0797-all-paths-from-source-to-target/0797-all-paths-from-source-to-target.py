b617c56b28e631d48f64c6f18bd0344c78babff7
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2022-12-29 17:49:24 -0800

    Time: 95 ms (97.22%), Space: 15.7 MB (78.57%) - LeetHub

---
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -0,0 +1,20 @@
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        n = len(graph)
+        res = []
+        path = []
+        vis = [False]*n
+        def dfs(cur, n):
+            path.append(cur)
+            if cur == n-1:
+                res.append(path[:])
+                path.pop()
+                return
+            vis[cur] = True
+            for ne in graph[cur]:
+                if not vis[ne]:
+                    dfs(ne, n)
+            vis[cur] = False
+            path.pop()
+        dfs(0, n)
+        return res
