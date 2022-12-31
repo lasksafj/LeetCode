fef4d6ca151af2f1f4c58ceee15b524070943fc7
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2022-12-30 19:21:37 -0800

    Time: 57 ms (91.37%), Space: 13.9 MB (93.68%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.py
+++ 0980-unique-paths-iii/0980-unique-paths-iii.py
@@ -1,23 +1,20 @@
 class Solution:
     def uniquePathsIII(self, grid: List[List[int]]) -> int:
-        
         def sol(x,y,k,m,n):
             if grid[x][y] == 2:
                 if k == 0:
                     res[0] += 1
                 return
-            vis[x][y] = True
+            grid[x][y] = 1
             for a,b in [[1,0],[-1,0],[0,1],[0,-1]]:
                 nx,ny = x+a,y+b
                 if nx>=0 and ny>=0 and nx<m and ny<n \
-                    and (grid[nx][ny] == 0 or grid[nx][ny] == 2) \
-                    and not vis[nx][ny]:
+                    and (grid[nx][ny] == 0 or grid[nx][ny] == 2):
                     sol(nx,ny,k-1,m,n)
-            vis[x][y] = False
+            grid[x][y] = 0
             
         m,n = len(grid),len(grid[0])
         res = [0]
-        vis = [[False]*n for _ in range(m)]
         x,y,k = 0,0,0
         for i in range(m):
             for j in range(n):
@@ -25,6 +22,5 @@ class Solution:
                     x,y = i,j
                 elif grid[i][j] == 0:
                     k += 1
-        vis[x][y] = True
         sol(x,y,k+1,m,n)
         return res[0]
