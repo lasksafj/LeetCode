9e6767fccf7cf299b355a31e1906722879a2efa3
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-01-12 21:14:17 -0800

    Time: 3795 ms (9.37%), Space: 13.8 MB (98.96%) - LeetHub

---
+++ 0483-smallest-good-base/0483-smallest-good-base.py
@@ -0,0 +1,23 @@
+class Solution:
+    def smallestGoodBase(self, n: str) -> str:
+        def check(k, p):
+            a = 0
+            i = 0
+            while i < p:
+                a += k**i
+                i += 1
+            return a
+        
+        n = int(n)
+        for i in range(60,0,-1):
+            l,r = 2,n
+            while l <= r:
+                m = (l+r)//2
+                y = check(m, i)
+                if y < n:
+                    l = m+1
+                elif y > n:
+                    r = m-1
+                else:
+                    return str(m)
+        return str(r)
