b1199abf75fca6080a5fb04c0abe4c8f93db9d74
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-01-12 23:12:28 -0800

    Time: 5056 ms (20.86%), Space: 84.5 MB (89.68%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
@@ -1,24 +1,11 @@
+from graphlib import TopologicalSorter
 class Solution:
     def longestPath(self, parent: List[int], s: str) -> int:
-        child = defaultdict(list)
-        n = len(parent)
-        for i in range(1, n):
-            child[parent[i]].append(i)
-
-        res = [0]
-        def dfs(cur):
-            a,b = 0,0
-            for ch in child[cur]:
-                c = dfs(ch)
-                if s[ch] != s[cur]:
-                    if c > a:
-                        b = a
-                        a = c
-                    elif c > b:
-                        b = c
-            
-            res[0] = max(res[0], a+b+1)
-            return a+1
-        dfs(0)
-        return res[0]
-            
+        def lengths():
+            downs = [[0] for _ in s]
+            ts = TopologicalSorter(dict(enumerate(zip(parent))))
+            for node in [*ts.static_order()][:0:-1]:
+                yield 1 + sum(nlargest(2, downs[node]))
+                if s[node] != s[parent[node]]:
+                    downs[parent[node]].append(1 + max(downs[node]))
+        return max(lengths())
