461add5c6082078a715947ce18ae402418f1d41d
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-01-22 18:26:12 -0800

    Time: 942 ms (40.57%), Space: 19.1 MB (8.15%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -1,11 +1,10 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        ind = defaultdict(int)
-        outd = defaultdict(int)
+        m = defaultdict(int)
         for a,b in trust:
-            ind[b] += 1
-            outd[a] += 1
+            m[b] += 1
+            m[a] -= 1
         for i in range(1,n+1):
-            if ind[i] == n-1 and outd[i] == 0:
+            if m[i] == n-1:
                 return i
         return -1
