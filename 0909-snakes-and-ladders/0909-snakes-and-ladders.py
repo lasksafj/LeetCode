60a9767f569d459e4fdff072411fa006f85f61f1
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-01-24 00:06:41 -0800

    Time: 376 ms (12.19%), Space: 14 MB (67.56%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.py
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.py
@@ -2,7 +2,7 @@ class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
         n = len(board)
         m = [-1] * (n*n+1)
-        dist = [inf] * (n*n+1)
+        
         d = 1
         k = -n
         for i in range(n-1,-1,-1):
@@ -14,15 +14,15 @@ class Solution:
                 m[k] = board[i][j]
                 k += d
             d *= -1
-        pq = [[0,1]]
-        while pq:
-            d,cur = heapq.heappop(pq)
-            if cur == n*n:
-                return d
+        dist = [-1] * (n*n+1)
+        q = deque([1])
+        dist[1] = 0
+        while q:
+            cur = q.popleft()
             for ne in range(cur+1, min(cur+6, n*n)+1):
                 if m[ne] != -1:
                     ne = m[ne]
-                if dist[ne] > d+1:
-                    dist[ne] = d+1
-                    heapq.heappush(pq, [d+1, ne])
-        return dist[n*n] if dist[n*n] != inf else -1
+                if dist[ne] == -1:
+                    dist[ne] = dist[cur]+1
+                    q.append(ne)
+        return dist[n*n]
