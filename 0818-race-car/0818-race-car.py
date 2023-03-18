e1d70b5e74ffba10eca2e129f9875a34318ba8c0
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-03-17 23:20:16 -0700

    Time: 38 ms (94.75%), Space: 14.5 MB (57.23%) - LeetHub

--- 0818-race-car/0818-race-car.py
+++ 0818-race-car/0818-race-car.py
@@ -1,13 +1,13 @@
 class Solution:
     def racecar(self, target: int) -> int:
-        pq = [[0, 0, 1]]
+        q = deque([[0, 0, 1]])
         m = defaultdict(int)
-        while pq:
-            step,cur,speed = heapq.heappop(pq)
+        while q:
+            step,cur,speed = q.popleft()
             if cur == target:
                 return step
-            heapq.heappush(pq, [step+1, cur+speed, speed*2])
+            q.append([step+1, cur+speed, speed*2])
             if (cur+speed > target and speed > 0) or (cur+speed < target and speed < 0):
-                heapq.heappush(pq, [step+1, cur, -speed//abs(speed)])
+                q.append([step+1, cur, -speed//abs(speed)])
 
         return 1
