0f993be78adeb62bf91e67c525dbdfb41cb1d148
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-03-21 17:57:36 -0700

    Time: 2562 ms (25.50%), Space: 82.3 MB (12.42%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -1,17 +1,16 @@
 class Solution:
     def minScore(self, n: int, roads: List[List[int]]) -> int:
-        adj = [defaultdict(int) for _ in range(n+1)]
+        adj = defaultdict(list)
         for a,b,c in roads:
-            adj[a][b] = c
-            adj[b][a] = c
-        res = inf
-        q = deque([1])
-        vis = set()
-        while q:
-            a = q.popleft()
-            for ne in adj[a]:
-                res = min(res, adj[a][ne])
-                if ne not in vis:
-                    q.append(ne)
-                    vis.add(ne)
-        return res
+            adj[a].append([b,c])
+            adj[b].append([a,c])
+        pq = [[100000, 1]]
+        vis = [1000001]*(n+1)
+        while pq:
+            v,c = heappop(pq)
+            for ne,d in adj[c]:
+                d = min(d,v)
+                if vis[ne] > d:
+                    vis[ne] = d
+                    heappush(pq, [d,ne])
+        return vis[n]
