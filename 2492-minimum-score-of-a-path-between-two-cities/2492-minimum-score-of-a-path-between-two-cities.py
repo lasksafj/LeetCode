07ee99ac39a4b901f160143e37ecc2057b5d8ef3
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-03-21 18:02:30 -0700

    Time: 1789 ms (66.11%), Space: 71.6 MB (52.68%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -4,13 +4,14 @@ class Solution:
         for a,b,c in roads:
             adj[a].append([b,c])
             adj[b].append([a,c])
-        pq = [[100000, 1]]
-        vis = [1000001]*(n+1)
-        while pq:
-            v,c = heappop(pq)
+        vis = set()
+        q = deque([1])
+        res = 100001
+        while q:
+            c = q.popleft()
             for ne,d in adj[c]:
-                d = min(d,v)
-                if vis[ne] > d:
-                    vis[ne] = d
-                    heappush(pq, [d,ne])
-        return vis[n]
+                res = min(res, d)
+                if ne not in vis:
+                    q.append(ne)
+                    vis.add(ne)
+        return res
