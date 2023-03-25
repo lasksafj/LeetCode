0d1d0d45168390b6763b226f84a6751cf953548f
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-03-24 18:03:24 -0700

    Time: 2211 ms (56.94%), Space: 112.2 MB (11.39%) - LeetHub

--- 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.py
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.py
@@ -1,29 +1,23 @@
 class Solution:
     def countPairs(self, n: int, edges: List[List[int]]) -> int:
-        p = [i for i in range(n)]
-        r = [0]*n
-        def root(x):
-            if x == p[x]:
-                return x
-            return root(p[x])
-        def union(a,b):
-            a,b = root(a),root(b)
-            if r[a] < r[b]:
-                p[a] = b
-            elif r[a] > r[b]:
-                p[b] = a
-            else:
-                p[a] = b
-                r[b] += 1
+        adj = defaultdict(list)
         for a,b in edges:
-            union(a,b)
-        p = [root(i) for i in range(n)]
-        m = Counter(p)
-        if len(m) == 1:
-            return 0
+            adj[a].append(b)
+            adj[b].append(a)
+        vis = set()
+        def dfs(cur):
+            ans = 1
+            for ne in adj[cur]:
+                if ne not in vis:
+                    vis.add(ne)
+                    ans += dfs(ne)
+            return ans
         res = 0
         s = 0
-        for k in m:
-            res += s*m[k]
-            s += m[k]
+        for i in range(n):
+            if i not in vis:
+                vis.add(i)
+                a = dfs(i)
+                res += s*a
+                s += a
         return res
