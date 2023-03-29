81a316f0f5bd73688f4b61ea9a929b04c4081a15
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-03-28 18:42:28 -0700

    Time: 32 ms (98.10%), Space: 13.9 MB (77.85%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.py
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -1,16 +1,12 @@
 class Solution:
     def maxSatisfaction(self, satisfaction: List[int]) -> int:
-        satisfaction.sort()
-        @cache
-        def dfs(i, k):
-            # print(i,k)
-            if i == len(satisfaction):
-                return 0
-            # res = -99999
-            # for ne in range(i+1, len(satisfaction)+1):
-            #     res = max(res, satisfaction[i]*k + dfs(ne, k+1))
-                
-            return max(satisfaction[i]*k + dfs(i+1,k+1), dfs(i+1,k))
-            # return res
-        return dfs(0,1)
+        satisfaction.sort(reverse=True)
+        s,res = 0,0
+        for n in satisfaction:
+            s += n
+            if s < 0:
+                s = 0
+            res += s
+        return res
+        
         
