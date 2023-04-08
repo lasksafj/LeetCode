b1588bda7a3fb3aa1235589746b4af4d378b03de
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-04-07 23:52:28 -0700

    Time: 37 ms (79.14%), Space: 14.3 MB (97.60%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -10,20 +10,15 @@ class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
         if node == None:
             return node
-        m = {}
+        vis = {}
         q = deque([node])
-        vis = set()
-        vis.add(node.val)
+        vis[node.val] = Node(node.val)
         while q:
             cur = q.popleft()
-            ncur = Node(cur.val)
-            m[ncur.val] = (ncur,cur)
+            ncur = vis[cur.val]
             for ne in cur.neighbors:
                 if ne.val not in vis:
                     q.append(ne)
-                    vis.add(ne.val)
-        for v in m:
-            ncur,cur = m[v]
-            for ne in cur.neighbors:
-                ncur.neighbors.append(m[ne.val][0])
-        return m[node.val][0]
+                    vis[ne.val] = Node(ne.val)
+                ncur.neighbors.append(vis[ne.val])
+        return vis[node.val]
