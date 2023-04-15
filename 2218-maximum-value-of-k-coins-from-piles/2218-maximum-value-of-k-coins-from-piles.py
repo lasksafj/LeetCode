5588d902b0120644ff874fbf90bcd85675e08cea
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-04-15 00:29:40 -0700

    Time: 3416 ms (96.86%), Space: 14.5 MB (86.79%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.py
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.py
@@ -4,10 +4,12 @@ class Solution:
         if n == 1:
             return sum(piles[0][:k])
         dp,prev_dp = [0]*(k+1),[0]*(k+1)
+        s = 0
         for p in piles:
             for i in range(1, len(p)):
                 p[i] += p[i - 1]
-            for j in range(1,k+1):
+            s = min(s + len(p), k)
+            for j in range(1,s+1):
                 dp[j] = prev_dp[j]
                 for l in range(1, min(j, len(p))+1):
                     dp[j] = max(dp[j], prev_dp[j-l] + p[l-1])
