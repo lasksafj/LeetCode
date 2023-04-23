6e7f8c97ee03b1014c31dc657c35a8c39ebf109d
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-04-23 12:15:48 -0700

    Time: 3280 ms (8.22%), Space: 442.4 MB (5.48%) - LeetHub

--- 1416-restore-the-array/1416-restore-the-array.py
+++ 1416-restore-the-array/1416-restore-the-array.py
@@ -2,22 +2,15 @@ class Solution:
     def numberOfArrays(self, s: str, k: int) -> int:
         n = len(s)
         dp = [0] * (n+1)
-        dp[0] = 1
-        lenk = 0
-        kk = k
-        while kk > 0:
-            lenk += 1
-            kk //= 10
-        for i in range(n):
-            j = i
-            b = lenk
-            while j >= 0 and b > 0:
-                a = int(s[j:i+1])
-                if a > k:
-                    break
-                if s[j] != '0':
-                    dp[i+1] += dp[j]
-                j -= 1
-                b -= 1
-        return dp[n] % 1000000007
+        dp[n] = 1
+        for i in range(n-1, -1, -1):
+            if s[i] == '0':
+                continue
+            num = int(s[i])
+            j = i+1
+            while j <= n and num <= k:
+                dp[i] += dp[j]
+                num = num*10 + (int(s[j]) if j < n else 0)
+                j += 1
+        return dp[0] % 1000000007
                 
