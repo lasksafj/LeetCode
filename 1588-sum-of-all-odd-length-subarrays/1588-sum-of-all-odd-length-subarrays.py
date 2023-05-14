3f758c5b145ae00b8919204410e8fa3244071472
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-05-13 23:41:27 -0700

    Time: 39 ms (88.36%), Space: 16.2 MB (14.53%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.py
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.py
@@ -1,17 +1,10 @@
 class Solution:
     def sumOddLengthSubarrays(self, arr: List[int]) -> int:
         n = len(arr)
-        dp = [0]*n
-        dp_even_startat = [0]*n
-        dp_odd_startat = [0]*n
-        dp[0] = arr[0]
-        dp_odd_startat[0] = arr[0]
-        for i in range(1, n):
-            noeven = (i+1)//2
-            dp[i] = dp_even_startat[i-1] + arr[i]*(i+1-noeven) + dp[i-1]
-            dp_even_startat[i] = dp_odd_startat[i-1] + arr[i]*noeven
-            dp_odd_startat[i] = dp_even_startat[i-1] + arr[i]*(i+1-noeven)
-        
-        # print(dp_odd_startat)
-        # print(dp_even_startat)
-        return dp[-1]
+        res = 0
+        odd_s = 0
+        even_s = 0
+        for i in range(n):
+            odd_s,even_s = even_s + arr[i] * ((i+2)//2), odd_s + arr[i] * ((i+1)//2)
+            res += odd_s
+        return res
