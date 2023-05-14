15691ffb6a740acd088c1cf9ada152ac9b3e3494
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-05-14 15:27:43 -0700

    Time: 2397 ms (56.33%), Space: 25.8 MB (18.99%) - LeetHub

--- 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.py
+++ 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.py
@@ -1,19 +1,11 @@
 class Solution:
     def maxScore(self, nums: List[int]) -> int:
-        res = [0,0]
-        path = []
         n = len(nums)
-        
-        def sol():
-            res[1] += 1
-            # print(nums)
-            if len(path) == n//2:
-                tmp = path[:]
-                tmp.sort()
-                # print(tmp)
-                s = sum([(i+1)*e for i,e in enumerate(tmp)])
-                res[0] = max(res[0], s)
-                return
+        @cache
+        def sol(mask, no_pairs):
+            if no_pairs*2 == n:
+                return 0
+            res = 0
             for i in range(n):
                 if nums[i] == 0:
                     continue
@@ -22,11 +14,15 @@ class Solution:
                         continue
                     a,b = nums[i],nums[j]
                     nums[i],nums[j] = 0,0
-                    path.append(gcd(a,b))
-                    sol()
+                    no_pairs += 1
+                    mask ^= (1 << i)
+                    mask ^= (1 << j)
+                    res = max(res, (no_pairs)*gcd(a,b) + sol(mask, no_pairs))
                     nums[i],nums[j] = a,b
-                    path.pop()
-                break
-        sol()
-        # print(res[1])
-        return res[0]
+                    no_pairs -= 1
+                    mask ^= (1 << i)
+                    mask ^= (1 << j)
+                
+            return res
+        mask = pow(2,n)-1
+        return sol(mask, 0)
