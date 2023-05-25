76c6868009b0eeeb5d18648974e2cd3916d736e5
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-05-24 19:12:37 -0700

    Time: 957 ms (18.85%), Space: 17 MB (70.64%) - LeetHub

---
+++ 2662-minimum-cost-of-a-path-with-special-roads/2662-minimum-cost-of-a-path-with-special-roads.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minimumCost(self, start: List[int], target: List[int], specialRoads: List[List[int]]) -> int:
+        n = len(specialRoads)
+        vis = defaultdict(int)
+        vis[(target[0],target[1])] = abs(target[0]-start[0])+abs(target[1]-start[1])
+        for a,b,c,d,e in specialRoads:
+            vis[(c,d)] = abs(c-start[0])+abs(d-start[1])
+        
+        pq = [(0,start[0],start[1])]
+        while pq:
+            cost,x,y = heappop(pq)
+            for a,b,c,d,e in specialRoads:
+                if x!=c or y!=d:
+                    w = min(abs(a-x)+abs(b-y)+e, abs(c-x)+abs(d-y))
+                    if cost+w < vis[(c,d)]:
+                        heappush(pq, (cost+w, c,d))
+                        vis[(c,d)] = cost+w
+        res = vis[(target[0],target[1])]
+        for a,b,c,d,e in specialRoads:
+            res = min(res, vis[(c,d)] + abs(target[0]-c)+abs(target[1]-d))
+        return res
