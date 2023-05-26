b74cd137da482b5f97b25543509fb85c5fe6b9d2
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-05-26 01:53:09 -0700

    Time: 5739 ms (44.44%), Space: 76.3 MB (5.56%) - LeetHub

--- 1728-cat-and-mouse-ii/1728-cat-and-mouse-ii.py
+++ 1728-cat-and-mouse-ii/1728-cat-and-mouse-ii.py
@@ -2,35 +2,33 @@ class Solution:
     def canMouseWin(self, grid: List[str], catJump: int, mouseJump: int) -> bool:
         @cache
         def dfs(mx,my,cx,cy,turn,mturn):
-            res = -1
-            if mx>=0 and my>=0 and mx<m and my<n and cx>=0 and cy>=0 and cx<m and cy<n and grid[mx][my] != '#' and grid[cx][cy] != '#':
-                if (mx==cx and my==cy) or grid[cx][cy] == 'F' or turn == 0:
-                    return False
-                if grid[mx][my] == 'F':
-                    return True
-                if mturn:
-                    res = False
-                    for d in [[1,0],[-1,0],[0,1],[0,-1]]:
-                        for j in range(mouseJump+1):
-                            nmx,nmy = mx+d[0]*j, my+d[1]*j
-                            if nmx>=0 and nmy>=0 and nmx<m and nmy<n and grid[nmx][nmy]=='#':
-                                break
-                            a = dfs(nmx,nmy,cx,cy,turn-1,mturn^1)
-                            if a == True:
-                                return True
-                    return False
-                else:
-                    res = True
-                    for d in [[1,0],[-1,0],[0,1],[0,-1]]:
-                        for j in range(catJump+1):
-                            ncx,ncy = cx+d[0]*j, cy+d[1]*j
-                            if ncx>=0 and ncy>=0 and ncx<m and ncy<n and grid[ncx][ncy]=='#':
-                                break
-                            a = dfs(mx,my,ncx,ncy,turn,mturn^1)
-                            if a == False:
-                                return False
-                    return True
-            return res
+            if (mx==cx and my==cy) or grid[cx][cy] == 'F' or turn == 0:
+                return False
+            if grid[mx][my] == 'F':
+                return True
+            if mturn:
+                res = False
+                for d in [[1,0],[-1,0],[0,1],[0,-1]]:
+                    for j in range(mouseJump+1):
+                        nmx,nmy = mx+d[0]*j, my+d[1]*j
+                        if nmx<0 or nmy<0 or nmx>=m or nmy>=n or grid[nmx][nmy]=='#':
+                            break
+                        a = dfs(nmx,nmy,cx,cy,turn-1,mturn^1)
+                        if a == True:
+                            return True
+                return False
+            else:
+                res = True
+                for d in [[1,0],[-1,0],[0,1],[0,-1]]:
+                    for j in range(catJump+1):
+                        ncx,ncy = cx+d[0]*j, cy+d[1]*j
+                        if ncx<0 or ncy<0 or ncx>=m or ncy>=n or grid[ncx][ncy]=='#':
+                            break
+                        a = dfs(mx,my,ncx,ncy,turn,mturn^1)
+                        if a == False:
+                            return False
+                return True
+                
         m,n = len(grid),len(grid[0])
         for i in range(m):
             for j in range(n):
