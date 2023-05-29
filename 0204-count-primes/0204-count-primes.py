c9e64bcda201083ab2b67130a2f7633cf4436082
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-05-28 23:34:00 -0700

    Time: 1308 ms (92.08%), Space: 94.3 MB (14.10%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -6,5 +6,5 @@ class Solution:
         prime[0]=prime[1]=False
         for i in range(2,int(sqrt(n))+1):
             if prime[i]:
-                prime[i*i:n:i] = [False] * len(prime[i*i:n:i])
+                prime[i+i:n:i] = [False] * len(prime[i+i:n:i])
         return sum(prime)
