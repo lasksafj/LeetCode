fa61d3646c69aae97bdbaeb9c9b3a3a5d60072bf
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-06-02 16:49:05 -0700

    Time: 2384 ms (75.76%), Space: 38.6 MB (15.15%) - LeetHub

--- 2612-minimum-reverse-operations/2612-minimum-reverse-operations.py
+++ 2612-minimum-reverse-operations/2612-minimum-reverse-operations.py
@@ -2,11 +2,11 @@ class Solution:
     def minReverseOperations(self, n: int, p: int, banned: List[int], k: int) -> List[int]:
         ban_set = set(banned)
         from sortedcontainers import SortedList
-        good = [SortedList() for _ in range(2)]
+        good = [[] for _ in range(2)]
         res = [-1]*n
         for i in range(n):
             if i not in ban_set and i != p:
-                good[i&1].add(i)
+                good[i&1].append(i)
         q = deque([p])
         step = 0
         while q:
@@ -16,8 +16,10 @@ class Solution:
                 l = abs(cur-(k-1))
                 r = n-1-(abs(n-1-(cur+k-1)))
                 a = bisect_left(good[l&1], l)
+                aa = a
                 while a < len(good[l&1]) and good[l&1][a] <= r:
                     q.append(good[l&1][a])
-                    good[l&1].pop(a)
+                    a += 1
+                del good[l&1][aa:a]
             step += 1
         return res
