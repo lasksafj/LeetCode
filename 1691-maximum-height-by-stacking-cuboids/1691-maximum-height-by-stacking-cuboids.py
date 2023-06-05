9978689e168e7bee2d99a06f6fc3f6b2e2c8681a
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-06-04 21:39:44 -0700

    Time: 93 ms (72.16%), Space: 16.3 MB (62.89%) - LeetHub

--- 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.py
+++ 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.py
@@ -3,14 +3,12 @@ class Solution:
         from itertools import permutations
         cuboids = [sorted(cuboids[i]) for i in range(len(cuboids))]
         cuboids.sort()
-        @cache
-        def dfs(i, pw, pl, ph):
-            if i == len(cuboids):
-                return 0
-            res = 0
+        dp = [c[2] for c in cuboids]
+        for i in range(len(cuboids)):
             w,l,h = cuboids[i]
-            if pw <= w and pl <= l and ph <= h:
-                res = max(res, h + dfs(i+1, w, l, h))
-            res = max(res, dfs(i+1, pw, pl, ph))
-            return res
-        return dfs(0, 0, 0, 0)
+            for j in range(i):
+                pw,pl,ph = cuboids[j]
+                if pw <= w and pl <= l and ph <= h:
+                    dp[i] = max(dp[i], dp[j] + h)
+        return max(dp)
+                
