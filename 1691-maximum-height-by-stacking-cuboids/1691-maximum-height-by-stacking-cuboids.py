7e0c44613d13ae304f1184b385a628f3b596d880
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-06-04 21:35:18 -0700

    Time: 168 ms (17.53%), Space: 20.1 MB (6.19%) - LeetHub

--- 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.py
+++ 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.py
@@ -1,26 +1,16 @@
 class Solution:
     def maxHeight(self, cuboids: List[List[int]]) -> int:
         from itertools import permutations
-        # cuboids = [sorted(cuboids[i]) for i in range(len(cuboids))]
-        # cuboids.sort(key=lambda x: max(x[0]*x[1],x[0]*x[2],x[1]*x[2]) )
-        # print(cuboids)
-        vis = [False]*len(cuboids)
+        cuboids = [sorted(cuboids[i]) for i in range(len(cuboids))]
+        cuboids.sort()
         @cache
-        def dfs(pw, pl, ph):
+        def dfs(i, pw, pl, ph):
+            if i == len(cuboids):
+                return 0
             res = 0
-            for i in range(len(cuboids)):
-                if not vis[i]:
-                    vis[i] = True
-                    for w,l,h in list(permutations(cuboids[i])):
-                        if pw <= w and pl <= l and ph <= h:
-                            res = max(res, h + dfs(w, l, h))
-                    vis[i] = False
-            # res = max(res, dfs(pw, pl, ph))
+            w,l,h = cuboids[i]
+            if pw <= w and pl <= l and ph <= h:
+                res = max(res, h + dfs(i+1, w, l, h))
+            res = max(res, dfs(i+1, pw, pl, ph))
             return res
-        return dfs(0, 0, 0)
-        
-        # a = dfs(0, 0, 0, 0)
-        # cuboids.sort(key=lambda x:min(x[0],x[1],x[2]))
-        # # print(cuboids)
-        # b = dfs(0, 0, 0, 0)
-        # return max(a,b)
+        return dfs(0, 0, 0, 0)
