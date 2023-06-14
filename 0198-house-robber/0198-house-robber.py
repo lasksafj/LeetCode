8fa6d5e1e3dbfabc68cb4f5fb216abcac8f47b0b
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-06-13 18:14:20 -0700

    Time: 54 ms (14.10%), Space: 16.2 MB (78.74%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,7 +1,7 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        dp = [0] * 4
-        for i in range(n):
-            dp[i%4] = max(dp[(i-2)%4] if i-2>=0 else 0, dp[(i-3)%4] if i-3>=0 else 0) + nums[i]
-        return max(dp[(n-1)%4], dp[(n-2)%4])
+        dp = [0] * n
+        for i,v in enumerate(nums):
+            dp[i] = max((dp[i-2] if i>=2 else 0) + v, dp[i-1] if i>=1 else 0)
+        return dp[n-1]
