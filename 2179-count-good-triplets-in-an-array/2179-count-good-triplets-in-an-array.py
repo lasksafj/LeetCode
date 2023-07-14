e83270bcb039ced527657416d8a6d6c13c5df78f
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-07-14 09:59:33 -0700

    Time: 1595 ms (90.48%), Space: 43.6 MB (33.33%) - LeetHub

--- 2179-count-good-triplets-in-an-array/2179-count-good-triplets-in-an-array.py
+++ 2179-count-good-triplets-in-an-array/2179-count-good-triplets-in-an-array.py
@@ -1,31 +1,18 @@
 class Solution:
     def goodTriplets(self, nums1: List[int], nums2: List[int]) -> int:
-        def getSum(i, T):
-            s = 0
-            i += 1
-            while i > 0:
-                s += T[i]
-                i -= (i&-i)
-            return s
-        def update(i, v, T):
-            i += 1
-            while i < len(T):
-                T[i] += v
-                i += (i&-i)
-        A = [0]*len(nums1)
+        from sortedcontainers import SortedList
         m = {}
         for i,n in enumerate(nums2):
             m[n] = i
-        T = [0]*(len(nums1)+1)
-        for i,n in enumerate(nums1):
-            A[i] = getSum(m[n], T)
-            update(m[n], 1, T)
-        res = 0
-        T = [0]*(len(nums1)+1)
+        A = SortedList()
         s = 0
-        for i in range(len(nums1)-1, -1, -1):
-            n = nums1[i]
-            res += (s-getSum(m[n], T)) * A[i]
-            update(m[n], 1, T)
+        res = 0
+        A.add(m[nums1[0]])
+        for n in nums1[1:]:
             s += 1
+            A.add(m[n])
+            l = A.bisect_left(m[n])
+            # print(l, len(nums1)-1-m[n]-(s-l))
+            res += l * (len(nums1)-1-m[n]-(s-l))
+            
         return res
