25e51cd08f19fe980ea9628c60dde5f395b70448
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-07-17 12:04:35 -0700

    Time: 946 ms (92.35%), Space: 30.8 MB (92.35%) - LeetHub

--- 2289-steps-to-make-array-non-decreasing/2289-steps-to-make-array-non-decreasing.py
+++ 2289-steps-to-make-array-non-decreasing/2289-steps-to-make-array-non-decreasing.py
@@ -1,9 +1,12 @@
 class Solution:
     def totalSteps(self, nums: List[int]) -> int:
-        dp = [0]*len(nums) #no numbers that nums[i] can eat on its right
+        dp = [0]*len(nums) 
         st = []
-        for i in range(len(nums)-1,-1,-1):
-            while st and nums[st[-1]] < nums[i]:
-                dp[i] = max(dp[i]+1, dp[st.pop()])
+        for i in range(len(nums)):
+            cur = 0
+            while st and nums[st[-1]] <= nums[i]:
+                cur = max(cur, dp[st.pop()])
+            if st:
+                dp[i] = cur+1
             st.append(i)
         return max(dp)
