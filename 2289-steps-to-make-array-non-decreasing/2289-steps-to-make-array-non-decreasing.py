6184606d29457a59b306d6ad120988877a6f22c4
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-07-17 11:55:47 -0700

    Time: 932 ms (97.06%), Space: 30.5 MB (97.06%) - LeetHub

--- 2289-steps-to-make-array-non-decreasing/2289-steps-to-make-array-non-decreasing.py
+++ 2289-steps-to-make-array-non-decreasing/2289-steps-to-make-array-non-decreasing.py
@@ -3,7 +3,6 @@ class Solution:
         dp = [0]*len(nums) #no numbers that nums[i] can eat on its right
         st = []
         for i in range(len(nums)-1,-1,-1):
-            cur = 1
             while st and nums[st[-1]] < nums[i]:
                 dp[i] = max(dp[i]+1, dp[st.pop()])
             st.append(i)
