c086122b0f84bcd3635bcff574ca69d38d4c62fa
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-07-17 21:13:05 -0700

    Time: 1477 ms (98.06%), Space: 40.9 MB (98.06%) - LeetHub

--- 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.py
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.py
@@ -1,26 +1,9 @@
 class Solution:
     def maximumImportance(self, n: int, roads: List[List[int]]) -> int:
-        adj = defaultdict(list)
-        for a,b in roads:
-            adj[a].append(b)
-            adj[b].append(a)
         A = [0]*n
-        k = n
-        for _,i in sorted([(len(adj[i]),i) for i in adj], reverse=True):
-            A[i] = k
-            k -= 1
-        vis = set()
-        # print(A)
-        def dfs(i):
-            res = 0
-            if i in vis:
-                return 0
-            vis.add(i)
-            for ne in adj[i]:
-                res += dfs(ne) + A[i] + A[ne]
-            return res
-        res = 0
-        for i in range(n):
-            if i not in vis:
-                res += dfs(i)
-        return res//2
+        for a,b in roads:
+            A[a] += 1
+            A[b] += 1
+        A.sort()
+        return sum([(i+1)*A[i] for i in range(n)])
+        
