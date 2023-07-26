0163cb3f57d7153dfb76f3aa82afd693a2879609
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-07-26 00:28:13 -0700

    Time: 4053 ms (26.92%), Space: 697.9 MB (32.69%) - LeetHub

--- 1883-minimum-skips-to-arrive-at-meeting-on-time/1883-minimum-skips-to-arrive-at-meeting-on-time.py
+++ 1883-minimum-skips-to-arrive-at-meeting-on-time/1883-minimum-skips-to-arrive-at-meeting-on-time.py
@@ -10,7 +10,6 @@ class Solution:
                 return 0
             h = dist[i]/speed
             return min(ceil(dfs(i+1, k) + h - eps), dfs(i+1, k-1) + h)
-        print(dfs(0,7))
         for k in range(N+1):
             if dfs(0,k) <= hoursBefore:
                 return k
