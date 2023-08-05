20ef600a40eb733946837e63a10fd0946f92c4ed
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-08-04 20:28:58 -0700

    Time: 923 ms (97.17%), Space: 41.6 MB (29.25%) - LeetHub

--- 2271-maximum-white-tiles-covered-by-a-carpet/2271-maximum-white-tiles-covered-by-a-carpet.py
+++ 2271-maximum-white-tiles-covered-by-a-carpet/2271-maximum-white-tiles-covered-by-a-carpet.py
@@ -1,24 +1,18 @@
 class Solution:
     def maximumWhiteTiles(self, tiles: List[List[int]], carpetLen: int) -> int:
         tiles.sort()
-        res = 0
-        A = [0]*len(tiles)
-        A[0] = tiles[0][1]-tiles[0][0]+1
-        for i in range(1, len(tiles)):
-            a,b = tiles[i]
-            A[i] = A[i-1] + b-a+1
-        # print(A)
-        for i in range(len(tiles)):
-            
-            a,b = tiles[i]
-            if b-a+1 >= carpetLen:
-                return carpetLen
-            x = max(0,b-carpetLen)
-            p = max(0, bisect_right(tiles, [x,inf]) - 1)
-            res = max(res, A[i] - A[p] + max(0, tiles[p][1] - max(x, tiles[0][0]-1)))
-            
-            # print(i)
-            # print('---- x',x)
-            # print('---- p',p)
-            # print('---- res',res)
-        return res
+        l, r = 0, 0
+        n = len(tiles)
+        window = 0
+        ans = 0
+        while l < n and r < n:
+            if tiles[l][0] + carpetLen > tiles[r][1]:
+                window += tiles[r][1] - tiles[r][0] + 1
+                ans = max(ans, window)
+                r += 1
+            else:
+                a = window + tiles[l][0] + carpetLen - tiles[r][0]
+                ans = max(ans,a)
+                window -= tiles[l][1] - tiles[l][0] + 1
+                l += 1
+        return ans
