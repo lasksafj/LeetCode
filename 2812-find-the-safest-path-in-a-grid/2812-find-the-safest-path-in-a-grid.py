e94c3f3737eb38f0eafb45d2ac05c4b00cd7f28d
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-08-05 21:10:37 -0700

    Time: 3390 ms (5.56%), Space: 27.2 MB (5.56%) - LeetHub

---
+++ 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.py
@@ -0,0 +1,40 @@
+class Solution:
+    def maximumSafenessFactor(self, grid: List[List[int]]) -> int:
+        def dist(a,b,c,d):
+            return abs(a-c) + abs(b-d)
+        
+        q = []
+        n = len(grid)
+        A = [[inf]*n for _ in range(n)]
+        vis = [[0]*n for _ in range(n)]
+        for i in range(n):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    if (i==0 and j==0) or (i==n-1 and j==n-1):
+                        return 0
+                    A[i][j] = 0
+                    q.append((0,i,j))
+        while q:
+            s,i,j = heappop(q)
+            for ni,nj in [[i+1,j],[i-1,j],[i,j+1],[i,j-1]]:
+                if 0<=ni<n and 0<=nj<n and A[ni][nj] == inf:
+                    A[ni][nj] = s+1
+                    heappush(q, (s+1,ni,nj))
+
+        q = [(-A[0][0],0,0)]
+        vis = [[0]*n for _ in range(n)]
+        vis[0][0] = 1
+        while q:
+            # print(q)
+            s,i,j = heappop(q)
+            s = -s
+            if i==n-1 and j==n-1:
+                return s
+
+            for ni,nj in [[i+1,j],[i-1,j],[i,j+1],[i,j-1]]:
+                if 0<=ni<n and 0<=nj<n and vis[ni][nj] == 0:
+                    d = min(s, A[ni][nj])
+                    heappush(q, (-d, ni,nj))
+                    vis[ni][nj] = 1
+            
+        return 0
