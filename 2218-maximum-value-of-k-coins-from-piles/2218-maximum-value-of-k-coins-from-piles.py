79826676d991e4191ce0cce4765142a21572568f
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-08-10 15:10:54 -0700

    Time: 4102 ms (94.50%), Space: 350.3 MB (12.30%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.py
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.py
@@ -1,18 +1,15 @@
 class Solution:
     def maxValueOfCoins(self, piles: List[List[int]], k: int) -> int:
-        n = len(piles)
-        if n == 1:
-            return sum(piles[0][:k])
-        dp,prev_dp = [0]*(k+1),[0]*(k+1)
-        s = 0
-        for p in piles:
-            for i in range(1, len(p)):
-                p[i] += p[i - 1]
-            s = min(s + len(p), k)
-            for j in range(1,s+1):
-                dp[j] = prev_dp[j]
-                for l in range(1, min(j, len(p))+1):
-                    dp[j] = max(dp[j], prev_dp[j-l] + p[l-1])
-            prev_dp = dp[:]
-        # print(dp)
-        return dp[k]
+        @cache
+        def dfs(i,k):
+            if k == 0:
+                return 0
+            if i == len(piles):
+                return 0
+            s = 0
+            res = dfs(i+1, k)
+            for j in range(min(k,len(piles[i]))):
+                s += piles[i][j]
+                res = max(res, dfs(i+1, k-j-1) + s)
+            return res
+        return dfs(0,k)
