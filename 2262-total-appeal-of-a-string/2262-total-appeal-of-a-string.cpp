b249859d1a2ca27219c9829417a6e6af0a3dc9ce
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-08-16 13:40:22 -0700

    Time: 61 ms (26.66%), Space: 10.7 MB (28.41%) - LeetHub

--- 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.cpp
+++ 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.cpp
@@ -2,19 +2,17 @@ class Solution {
 public:
     long long appealSum(string s) {
         unordered_map<char,int> prev;
-        vector<int> dp(s.size());
+        vector<int> dp(2);
         dp[0] = 1;
+        long long res = 1;
         prev[s[0]] = 0;
         for (int i = 1; i < s.size(); i++) {
             if (prev.find(s[i]) == prev.end()) {
                 prev[s[i]] = -1;
             }
-            dp[i] = dp[i-1] + i - prev[s[i]];
+            dp[i%2] = dp[(i-1)%2] + i - prev[s[i]];
             prev[s[i]] = i;
-        }
-        long long res = 0;
-        for (int n: dp) {
-            res += n;
+            res += dp[i%2];
         }
         return res;
     }
