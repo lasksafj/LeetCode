b154b52555c3cb2f9768cccc643a7a719fd2114e
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-08-17 13:23:24 -0700

    Time: 660 ms (76.14%), Space: 18.1 MB (83.99%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -1,13 +1,12 @@
 class Solution:
     def minInsertions(self, s: str) -> int:
-        sr = s[::-1]
-        n = len(s)
-        lcsubseq = [[0]*(n+1) for _ in range(n+1)]
-        for i in range(1, n+1):
-            for j in range(1, n+1):
-                if s[i-1] == sr[j-1]:
-                    lcsubseq[i][j] = 1 + lcsubseq[i-1][j-1]
+#         longest palindromic subsequence in s
+        dp = [[1]*len(s) for _ in range(len(s))]
+        for l in range(2, len(s)+1):
+            for i in range(len(s)-l+1):
+                j = i+l-1
+                if j-i >= 2:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j-1], dp[i+1][j-1] + (s[i] == s[j])*2)
                 else:
-                    lcsubseq[i][j] = max(lcsubseq[i-1][j], lcsubseq[i][j-1])
-        return n - lcsubseq[n][n]
-        
+                    dp[i][j] = 2 if s[i] == s[j] else 1
+        return len(s) - dp[0][len(s)-1]
