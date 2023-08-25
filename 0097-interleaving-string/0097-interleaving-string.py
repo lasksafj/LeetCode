5e691e4ee30f94a72f1ce36c76ce68f2b2b61350
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-08-24 17:45:20 -0700

    Time: 58 ms (42.62%), Space: 16.3 MB (92.90%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -2,13 +2,12 @@ class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         if len(s3) != len(s1)+len(s2):
             return False
-        @cache
-        def dfs(i,j,k):
-            if i == len(s1) and j == len(s2) and k == len(s3):
-                return True
-            if i < len(s1) and s1[i] == s3[k] and dfs(i+1,j,k+1):
-                return True
-            if j < len(s2) and s2[j] == s3[k] and dfs(i,j+1,k+1):
-                return True
-            return False
-        return dfs(0,0,0)
+        dp = [False] * (len(s2)+1)
+        dp[-1] = True
+        for i in range(len(s1),-1,-1):
+            for j in range(len(s2),-1,-1):
+                if i < len(s1):
+                    dp[j] = dp[j] & (s1[i] == s3[i+j])
+                if j < len(s2) and s2[j] == s3[i+j]:
+                    dp[j] |= dp[j+1]
+        return dp[0]
