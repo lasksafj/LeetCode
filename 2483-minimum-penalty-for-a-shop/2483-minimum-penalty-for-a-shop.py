25ccc356ed434d1deffdcb3ce9eddc80f382bd4e
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-08-29 00:38:33 -0700

    Time: 220 ms (33.74%), Space: 21.2 MB (37.05%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.py
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.py
@@ -1,19 +1,16 @@
 class Solution:
     def bestClosingTime(self, customers: str) -> int:
-        n = len(customers)
-        left = [0]*n
-        right = [0]*n
-        left[0] = 0+(customers[0] == 'N')
-        for i in range(1,n):
-            left[i] = left[i-1] + (customers[i] == 'N')
-        right[-1] = 0+(customers[-1] == 'Y')
-        for i in range(n-2,-1,-1):
-            right[i] = right[i+1] + (customers[i] == 'Y')
-        minpen = inf
-        res = 0
-        for i in range(n+1):
-            cur = (left[i-1] if i>0 else 0) + (right[i] if i<n else 0)
-            if minpen > cur:
-                minpen = cur
+        N = len(customers)
+        pre = [0]*N
+        pre[0] = (customers[0] == 'N')
+        for i in range(1,N):
+            pre[i] = pre[i-1] + (customers[i] == 'N')
+        p = 0
+        pen = pre[-1]
+        res = N
+        for i in range(N-1, -1, -1):
+            p += (customers[i] == 'Y')
+            if (pre[i-1] if i>0 else 0) + p <= pen:
+                pen = pre[i-1] + p
                 res = i
         return res
