884c01cddd37df0f4ac8251703421b5d26f9c9b4
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-09-03 12:53:37 -0700

    Time: 806 ms (80.00%), Space: 45.1 MB (60.00%) - LeetHub

--- 2845-count-of-interesting-subarrays/2845-count-of-interesting-subarrays.py
+++ 2845-count-of-interesting-subarrays/2845-count-of-interesting-subarrays.py
@@ -1,17 +1,11 @@
 class Solution:
     def countInterestingSubarrays(self, nums: List[int], modulo: int, k: int) -> int:
-        A = []
-        for n in nums:
-            if n%modulo == k:
-                A.append(1)
-            else:
-                A.append(0)
         res = 0
         mp = defaultdict(int)
         mp[0] = 1
         pre = 0
-        for n in A:
-            pre += n
+        for n in nums:
+            pre += (n%modulo == k)
             if pre >= modulo:
                 pre -= modulo
             res += mp[(pre-k+modulo)%modulo]
