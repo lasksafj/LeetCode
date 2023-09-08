5b943d5d36777ba517dd09b4fc01e75a0c5e8886
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-09-07 21:07:24 -0700

    Time: 2182 ms (25.53%), Space: 133.9 MB (10.64%) - LeetHub

--- 1687-delivering-boxes-from-storage-to-ports/1687-delivering-boxes-from-storage-to-ports.py
+++ 1687-delivering-boxes-from-storage-to-ports/1687-delivering-boxes-from-storage-to-ports.py
@@ -1,24 +1,24 @@
 class Solution:
     def boxDelivering(self, boxes: List[List[int]], portsCount: int, maxBoxes: int, maxWeight: int) -> int:
-        N = len(boxes)
-        dp = [0] + [inf]*N
-        trip = 0
-        prev_j = 0
-        j = 0
-        for i in range(N):
-            while j < N and maxBoxes > 0 and maxWeight >= boxes[j][1]:
-                maxBoxes -= 1
-                maxWeight -= boxes[j][1]
-                if j == 0 or boxes[j][0] != boxes[j-1][0]:
-                    trip += 1
-                    prev_j = j
+        n = len(boxes)
+
+        @lru_cache(None)
+        def backtrack(i):
+            if i > n-1: return 0
+
+            j = k = i
+            b = w = extra = 0
+            while j < n and b < maxBoxes and w + boxes[j][1] <= maxWeight:
+                b += 1
+                w += boxes[j][1]
+                if j != i and boxes[j][0] != boxes[j-1][0]:
+                    extra += 1
+                    k = j
                 j += 1
-            
-            dp[j] = min(dp[j], dp[i] + trip + 1)
-            dp[prev_j] = min(dp[prev_j], dp[i] + trip)
-            
-            maxBoxes += 1
-            maxWeight += boxes[i][1]
-            if i < N-1 and boxes[i][0] != boxes[i+1][0]:
-                trip -= 1
-        return dp[-1]
+
+            trip = 2 + extra + backtrack(j)
+            if k != i:
+                trip = min(trip, 1 + extra + backtrack(k))
+            return trip
+
+        return backtrack(0)
