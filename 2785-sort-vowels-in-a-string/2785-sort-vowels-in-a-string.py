9bae2674d3a62daf66f3e7e7d9bc10958b6a5385
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-11-12 16:22:14 -0800

    Time: 144 ms (64.72%), Space: 18.5 MB (94.66%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.py
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.py
@@ -1,17 +1,20 @@
 class Solution:
     def sortVowels(self, s: str) -> str:
         res = []
-        A = []
+        A = defaultdict(int)
+        sortedVowel = 'AEIOUaeiou'
         for c in s:
-            if c not in 'euoaiEUOAI':
+            if c not in sortedVowel:
                 res.append(c)
             else:
                 res.append(' ')
-                A.append(c)
-        A.sort()
+                A[c] += 1
+        
         j = 0
         for i,c in enumerate(res):
             if c == ' ':
-                res[i] = A[j]
-                j += 1
+                while A[sortedVowel[j]] == 0:
+                    j += 1
+                res[i] = sortedVowel[j]
+                A[sortedVowel[j]] -= 1
         return ''.join(res)
