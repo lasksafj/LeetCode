01910464f3d14886bae0c1eb95055f4bd5f18ccf
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-11-21 22:20:53 -0800

    Time: 802 ms (43.16%), Space: 37.5 MB (76.42%) - LeetHub

--- 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.py
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.py
@@ -3,13 +3,11 @@ class Solution:
         res = []
         q = deque([[0,0]])
         while q:
-            vis = set()
             for _ in range(len(q)):
                 a,b = q.popleft()
                 res.append(nums[a][b])
-                if a+1 < len(nums) and b < len(nums[a+1]) and (a+1,b) not in vis:
+                if b == 0 and a+1 < len(nums):
                     q.append([a+1,b])
                 if b+1 < len(nums[a]):
                     q.append([a,b+1])
-                    vis.add((a,b+1))
         return res
