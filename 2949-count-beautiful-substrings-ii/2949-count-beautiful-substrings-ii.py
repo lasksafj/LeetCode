b5a3620eed5e6e3513f94b2d694df9775a74f6d2
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2023-12-05 15:57:19 -0800

    Time: 213 ms (94.01%), Space: 17.5 MB (93.89%) - LeetHub

--- 2949-count-beautiful-substrings-ii/2949-count-beautiful-substrings-ii.py
+++ 2949-count-beautiful-substrings-ii/2949-count-beautiful-substrings-ii.py
@@ -2,17 +2,17 @@ class Solution:
     def beautifulSubstrings(self, s: str, k: int) -> int:
         N = len(s)
         m = defaultdict(lambda:defaultdict(int))
-        m[0][0] = 1
+        l = 1
+        for i in range(1, k+1):
+            if (i*i) % k == 0:
+                l = i*2
+                break
+        m[0][l-1] = 1
+        diff = 0
         res = 0
-        a,b = 0,0
         for i in range(N):
-            if s[i] in 'euoai':
-                a += 1
-            else:
-                b += 1
-            for j in m[a-b]:
-                if (a-j)*(a-j) % k == 0:
-                    res += m[a-b][j]
-            m[a-b][a%k] += 1
+            diff += 1 if s[i] in 'ueoai' else -1
+            res += m[diff][i%l]
+            m[diff][i%l] += 1
             
         return res
