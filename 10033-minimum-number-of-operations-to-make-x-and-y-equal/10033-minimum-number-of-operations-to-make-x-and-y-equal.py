6fc975d5308f92dc038735d92d2f1fa8a8563d20
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2024-01-06 15:31:56 -0800

    Time: 89 ms (100.00%), Space: 28.5 MB (100.00%) - LeetHub

--- 10033-minimum-number-of-operations-to-make-x-and-y-equal/10033-minimum-number-of-operations-to-make-x-and-y-equal.py
+++ 10033-minimum-number-of-operations-to-make-x-and-y-equal/10033-minimum-number-of-operations-to-make-x-and-y-equal.py
@@ -1,25 +1,12 @@
 class Solution:
     def minimumOperationsToMakeEqual(self, x: int, y: int) -> int:
-        if x <= y:
-            return y-x
-        ma = x + max(5-x%5, 11-x%11)
-        vis = [0]*(ma+1)
-        q = deque([x])
-        res = 0
-        while q:
-            # print(q)
-            for _ in range(len(q)):
-                a = q.popleft()
-                if a == y:
-                    return res
-                tmp = [a-1,a+1]
-                if a%11==0:
-                    tmp.append(a//11)
-                if a%5==0:
-                    tmp.append(a//5)
-                for n in tmp:
-                    if n <= ma and not vis[n]:
-                        vis[n] = 1
-                        q.append(n)
-            res += 1
-        return res
+        @cache
+        def dfs(x):
+            # print(x)
+            if x <= y:
+                return y-x
+            a = dfs((x+10)//11) + (11-x%11)%11
+            b = dfs((x+4)//5) + (5-x%5)%5
+            c = dfs(x-1)
+            return min(a,b,c)+1
+        return dfs(x)
