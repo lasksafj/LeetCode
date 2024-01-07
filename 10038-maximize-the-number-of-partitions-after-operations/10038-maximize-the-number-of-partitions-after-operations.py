bf1bddc2b47f390fa7ad57eda6a452f0bb990870
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2024-01-06 23:32:32 -0800

    Time: 840 ms (100.00%), Space: 218.2 MB (100.00%) - LeetHub

---
+++ 10038-maximize-the-number-of-partitions-after-operations/10038-maximize-the-number-of-partitions-after-operations.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxPartitionsAfterOperations(self, s: str, k: int) -> int:
+        @cache
+        def dfs(i,change,mask):
+            if i == len(s):
+                return 0
+            d = ord(s[i]) - ord('a')
+            mask2 = mask|(1<<d)
+            cnt = mask2.bit_count()
+            if cnt > k:
+                res = dfs(i+1,change,1<<d) + 1
+            else:
+                res = dfs(i+1,change,mask2)
+            if change:
+                for j in range(26):
+                    mask2 = mask|(1<<j)
+                    cnt = mask2.bit_count()
+                    if cnt > k:
+                        res = max(res, dfs(i+1,0,1<<j) + 1)
+                    else:
+                        res = max(res, dfs(i+1,0,mask2))
+            return res
+        
+        return dfs(0,1,0)+1
