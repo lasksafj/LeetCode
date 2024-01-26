c23643111563d5638cafb49cf64855fdab9e3554
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2024-01-25 19:56:11 -0800

    Time: 61 ms (61.21%), Space: 19.8 MB (99.70%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
@@ -6,55 +6,28 @@
 #         self.right = right
 class Solution:
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
-        if not root:
-            return None
-        if key == root.val:
-            if not root.left and not root.right:
+        def dfs(cur):
+            if not cur:
                 return None
-            elif not root.left:
-                return root.right
-            elif not root.right:
-                return root.left
-        cur = root
-        prev = None
-        while cur and cur.val != key:
-            prev = cur
-            if cur.val < key:
-                cur = cur.right
-            elif cur.val > key:
-                cur = cur.left
+            if cur.val > key:
+                cur.left = dfs(cur.left)
+            elif cur.val < key:
+                cur.right = dfs(cur.right)
             else:
-                break
-        if not cur:
-            return root
-        if not cur.left and not cur.right:
-            if cur == prev.left:
-                prev.left = None
-            else:
-                prev.right = None
-            cur = None
-        elif not cur.left:
-            if cur == prev.left:
-                prev.left = cur.right
-            else:
-                prev.right = cur.right
-            cur = None
-        elif not cur.right:
-            if cur == prev.left:
-                prev.left = cur.left
-            else:
-                prev.right = cur.left
-            cur = None
-        else:
-            x = cur.right
-            prevx = None
-            while x.left:
-                prevx = x
-                x = x.left
-            cur.val = x.val
-            if prevx:
-                prevx.left = x.right
-                x = None
-            else:
-                cur.right = x.right
-        return root
+                if not cur.left:
+                    return cur.right
+                elif not cur.right:
+                    return cur.left
+                else:
+                    x = cur.right
+                    prevx = None
+                    while x.left:
+                        prevx = x
+                        x = x.left
+                    cur.val = x.val
+                    if prevx:
+                        prevx.left = x.right
+                    else:
+                        cur.right = x.right
+            return cur
+        return dfs(root)
