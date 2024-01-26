9d5776ca746a1220649208e77b64a9b452084248
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2024-01-25 18:39:34 -0800

    Time: 69 ms (97.69%), Space: 16.7 MB (32.75%) - LeetHub

---
+++ 3015-count-the-number-of-houses-at-a-certain-distance-i/3015-count-the-number-of-houses-at-a-certain-distance-i.py
@@ -0,0 +1,33 @@
+class Solution:
+    def countOfPairs(self, n: int, x: int, y: int) -> List[int]:
+        if x > y:
+            x,y = y,x
+        A = [0]*(n+1)
+        for i in range(1,n+1):
+            A[1] += 2   # node i can go all left and right, so every distance += 2
+                        # 12i45, i->2:1, i->4:1, i->1:2, i->5:2, so res[1] += 2, res[2] += 2, ... 
+                        # so update range [1,n] += 2 using diff array A
+            
+            A[min(i-1, abs(i-y)+1+x-1) + 1] -= 1 
+                # i->1 then stop, 
+                # so all distance larger than distance i->1 -= 1
+                
+            A[min(n-i, abs(i-x)+1+n-y) + 1] -= 1
+                # i->n then stop
+                
+            A[min(abs(i-x), abs(i-y)+1) + 1] += 1
+                # i->x then split into 2 ways,
+                # so all distance after i->x += 1
+            
+            A[min(abs(i-y), abs(i-x)+1) + 1] += 1
+                # i->y then split into 2 ways
+                
+            r = max(x-i, 0) + max(i-y, 0)
+            A[r + (y-x)//2 + 1] -= 1
+            A[r + (y-x+1)//2 + 1] -= 1
+                # When go from x to y then go to the middle, it will stop in middle, 
+                # When go from y to x then go to the middle, it will stop in middle
+                
+        for i in range(2,n+1):
+            A[i] += A[i-1]
+        return A[1:]
