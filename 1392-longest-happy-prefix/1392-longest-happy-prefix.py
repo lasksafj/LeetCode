e8d36c8593eaf6f7b90f5cef972f88d22b9c75d5
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2024-02-04 01:10:27 -0800

    Time: 161 ms (79.87%), Space: 21.3 MB (44.02%) - LeetHub

---
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.py
@@ -0,0 +1,16 @@
+def prefix_function(s):
+    n = len(s)
+    pi = [0] * n
+    for i in range(1, n):
+        j = pi[i - 1]
+        while j > 0 and s[i] != s[j]:
+            j = pi[j - 1]
+        if s[i] == s[j]:
+            j += 1
+        pi[i] = j
+    return pi
+
+class Solution:
+    def longestPrefix(self, s: str) -> str:
+        return s[:prefix_function(s)[-1]]
+        
