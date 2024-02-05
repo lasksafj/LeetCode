22e329d842d03517ad19fa432f39ae98f372207c
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2024-02-04 18:49:33 -0800

    Time: 470 ms (100.00%), Space: 17.3 MB (100.00%) - LeetHub

--- 3027-find-the-number-of-ways-to-place-people-ii/3027-find-the-number-of-ways-to-place-people-ii.py
+++ 3027-find-the-number-of-ways-to-place-people-ii/3027-find-the-number-of-ways-to-place-people-ii.py
@@ -1,4 +1,4 @@
-from sortedcontainers import SortedList
+
 class Solution:
     def numberOfPairs(self, points: List[List[int]]) -> int:
         points.sort(key=lambda x:(x[0], -x[1]))
@@ -6,10 +6,11 @@ class Solution:
         res = 0
         for i in range(N):
             a,b = points[i]
-            A = SortedList()
+            largest_d = -inf
             for j in range(i+1,N):
                 c,d = points[j]
                 if d <= b:
-                    res += (A.bisect_right(b) - A.bisect_left(d) == 0)
-                A.add(d)
+                    if d > largest_d:
+                        res += 1
+                        largest_d = d
         return res
