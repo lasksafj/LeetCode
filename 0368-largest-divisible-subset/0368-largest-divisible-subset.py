cf92b677850ab58bc3df61883868a731b0550c03
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2024-02-08 17:59:18 -0800

    Time: 2107 ms (13.59%), Space: 178.5 MB (5.03%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.py
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.py
@@ -3,17 +3,17 @@ class Solution:
         nums.sort()
         N = len(nums)
         @cache
-        def dfs(i):
-            A = []
-            for j in range(i+1, N):
-                if nums[j]%nums[i] == 0:
-                    tmp = dfs(j)
-                    if len(tmp) > len(A):
-                        A = tmp
-            return [nums[i]] + A
+        def dfs(i,prev):
+            if i == N:
+                return []
+            A = dfs(i+1,prev)
+            B = []
+            if nums[i] % nums[prev] == 0:
+                B = [nums[i]] + dfs(i+1,i)
+            return A if len(A) > len(B) else B
         res = []
         for i in range(N):
-            tmp = dfs(i)
+            tmp = dfs(i,i)
             if len(tmp) > len(res):
                 res = tmp
         return res
