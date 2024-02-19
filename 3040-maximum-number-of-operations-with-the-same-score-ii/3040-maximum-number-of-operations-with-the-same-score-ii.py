6d966996ac3b588a62bd4c9bd190b87c78601021
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2024-02-19 01:07:30 -0800

    Time: 2085 ms (33.33%), Space: 362.7 MB (33.33%) - LeetHub

--- 3040-maximum-number-of-operations-with-the-same-score-ii/3040-maximum-number-of-operations-with-the-same-score-ii.py
+++ 3040-maximum-number-of-operations-with-the-same-score-ii/3040-maximum-number-of-operations-with-the-same-score-ii.py
@@ -1,25 +1,17 @@
 class Solution:
     def maxOperations(self, nums: List[int]) -> int:
         N = len(nums)
-        if N < 2:
-            return 0
         @cache
-        def go(i, j, x):
-            ans = 0
-            if i + 1 < j and nums[i] + nums[i+1] == x:
-                tmp = go(i+2, j, x) + x
-                if tmp > ans:
-                    ans = tmp
-            if j - 1 > i and nums[j] + nums[j-1] == x:
-                tmp = go(i, j-2, x) + x
-                if tmp > ans:
-                    ans = tmp
-            if i < j and nums[i] + nums[j] == x:
-                tmp = go(i+1, j-1, x) + x
-                if tmp > ans:
-                    ans = tmp
-            return ans
-        even = (go(1, N-2, nums[0] + nums[N-1]) + nums[0] + nums[N-1])//(nums[0] + nums[N-1])
-        left = (go(2, N-1, nums[0] + nums[1]) + nums[0] + nums[1])//(nums[0] + nums[1])
-        right = (go(0, N-3, nums[-1] + nums[-2]) + nums[-1] + nums[-2])//(nums[-1] + nums[-2])
-        return max(even, left, right)
+        def dfs(i,j,score):
+            res = 0
+            if i+1 < j and nums[i]+nums[i+1] == score:
+                res = max(res, dfs(i+2,j,score) + 1)
+            if i < j and nums[i]+nums[j] == score:
+                res = max(res, dfs(i+1,j-1,score) + 1)
+            if i+1 < j and nums[j]+nums[j-1] == score:
+                res = max(res, dfs(i,j-2,score) + 1)
+            return res
+
+        return max(dfs(2,N-1,nums[0]+nums[1]), 
+                   dfs(0,N-3,nums[-1]+nums[-2]),
+                   dfs(1,N-2,nums[0]+nums[-1])) + 1
