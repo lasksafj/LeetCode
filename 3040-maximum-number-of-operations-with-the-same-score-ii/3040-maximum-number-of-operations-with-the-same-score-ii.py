ff5b0c297e0c05f83618a76f13362f204cbca2b4
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2024-02-19 01:06:10 -0800

    Time: 2039 ms (33.33%), Space: 362.5 MB (33.33%) - LeetHub

--- 3040-maximum-number-of-operations-with-the-same-score-ii/3040-maximum-number-of-operations-with-the-same-score-ii.py
+++ 3040-maximum-number-of-operations-with-the-same-score-ii/3040-maximum-number-of-operations-with-the-same-score-ii.py
@@ -1,25 +1,17 @@
-def f(nums, score):
-    dp = [[0]*len(nums) for _ in range(len(nums))]
-
-    def dfs(i,j):
-        if i >= j:
-            return 0
-        if dp[i][j] > 0:
-            return dp[i][j]
-        res = 0
-        if i+1 < len(nums) and nums[i]+nums[i+1] == score:
-            res = max(res, dfs(i+2,j) + 1)
-        if nums[i]+nums[j] == score:
-            res = max(res, dfs(i+1,j-1) + 1)
-        if j >= 0 and nums[j]+nums[j-1] == score:
-            res = max(res, dfs(i,j-2) + 1)
-        dp[i][j] = res
-        return res
-    return dfs(0,len(nums)-1)
-
 class Solution:
     def maxOperations(self, nums: List[int]) -> int:
-        res = 0
-        for score in [nums[0]+nums[1],nums[0]+nums[-1],nums[-1]+nums[-2]]:
-            res = max(res, f(nums, score))
-        return res
+        N = len(nums)
+        @cache
+        def dfs(i,j,score):
+            res = 0
+            if i+1 < j and nums[i]+nums[i+1] == score:
+                res = max(res, dfs(i+2,j,score) + 1)
+            if i < j and nums[i]+nums[j] == score:
+                res = max(res, dfs(i+1,j-1,score) + 1)
+            if i+1 < j and nums[j]+nums[j-1] == score:
+                res = max(res, dfs(i,j-2,score) + 1)
+            return res
+
+        return max(dfs(2,N-1,nums[0]+nums[1]), 
+                   dfs(0,N-3,nums[-1]+nums[-2]),
+                   dfs(1,N-2,nums[0]+nums[-1])) + 1
