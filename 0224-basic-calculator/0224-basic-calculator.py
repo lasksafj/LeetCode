71a8cf978fc796a7ffe3e34043d9de845e873f33
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2024-04-13 00:49:37 -0700

    Time: 88 ms (27.65%), Space: 18 MB (84.47%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -0,0 +1,47 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        s = '(' + s + ')'
+        postfix = []
+        st = []
+        i = 0
+        prev_operator = ''
+        while i < len(s):
+            ch = s[i]
+            if ch == ' ':
+                i += 1
+                continue
+            if ch.isdigit():
+                prev_operator = ''
+                j = i+1
+                while j < len(s) and s[j].isdigit():
+                    j += 1
+                postfix.append(int(s[i:j]))
+                i = j-1
+            elif ch == '(':
+                prev_operator = '('
+                st.append(ch)
+            elif ch == ')':
+                prev_operator = ''
+                while st and st[-1] != '(':
+                    b = postfix.pop()
+                    a = postfix.pop()
+                    if st.pop() == '+':
+                        postfix.append(a+b)
+                    else:
+                        postfix.append(a-b)
+                st.pop()
+            else:
+                if st and st[-1] != '(':
+                    b = postfix.pop()
+                    a = postfix.pop()
+                    if st.pop() == '+':
+                        postfix.append(a+b)
+                    else:
+                        postfix.append(a-b)
+                if prev_operator == '(':
+                    postfix.append(0)
+                st.append(ch)
+                prev_operator = ''
+            i += 1
+            # print(postfix,st)
+        return postfix[0]
