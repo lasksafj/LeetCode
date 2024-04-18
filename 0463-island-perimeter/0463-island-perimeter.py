68c94143cffd3fba2cda9a9d91b46e256c393f12
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2024-04-17 19:31:41 -0700

    Time: 377 ms (63.43%), Space: 16.9 MB (65.66%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -0,0 +1,12 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        res = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]:
+                    res += 1 if i-1<0 or grid[i-1][j]==0 else 0
+                    res += 1 if i+1==len(grid) or grid[i+1][j]==0 else 0
+                    res += 1 if j-1<0 or grid[i][j-1]==0 else 0
+                    res += 1 if j+1==len(grid[0]) or grid[i][j+1]==0 else 0
+
+        return res
