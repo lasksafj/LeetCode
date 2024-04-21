cfb609a7a5b71f7795ea0c4329337892cd2f90c9
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2024-04-20 20:55:48 -0700

    Time: 869 ms (95.99%), Space: 46.1 MB (99.24%) - LeetHub

--- 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.py
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.py
@@ -4,13 +4,14 @@ class Solution:
         mask = 0
         for i in range(31,-1,-1):
             mask |= 1<<i
-            s = {n&mask for n in nums}
+            s = set()
             res = res|(1<<i)
             ok = False
             for n in nums:
                 if res^n & mask in s:
                     ok = True
                     break
+                s.add(n&mask)
             if not ok:
                 res = res&~(1<<i)
         return res
