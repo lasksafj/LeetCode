6aa8e366810ad3f0e5a73dc7a74bc16ba8c32c12
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2024-05-14 00:53:15 -0700

    Time: 2205 ms (76.22%), Space: 16.5 MB (71.32%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.py
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.py
@@ -1,16 +1,15 @@
 class Solution:
     def getMaximumGold(self, grid: List[List[int]]) -> int:
         M,N = len(grid),len(grid[0])
-        vis = [[False]*N for _ in range(M)]
         def dfs(i,j):
-            if i<0 or j<0 or i==M or j==N or vis[i][j] or grid[i][j]==0:
+            if i<0 or j<0 or i==M or j==N or grid[i][j]<=0:
                 return 0
-            vis[i][j] = True
+            grid[i][j] *= -1
             res = max(dfs(i+1,j),
                     dfs(i-1,j),
                     dfs(i,j+1),
                     dfs(i,j-1))
-            vis[i][j] = False
+            grid[i][j] *= -1
             return res + grid[i][j]
         res = 0
         for i in range(M):
