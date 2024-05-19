1744bf4b0bb47ae521a46251dba402a82768dbe8
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2024-05-18 17:39:33 -0700

    Time: 1244 ms (9.09%), Space: 32.6 MB (10.23%) - LeetHub

--- 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.py
+++ 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.py
@@ -4,25 +4,25 @@ class Solution:
         for a,b in edges:
             adj[a].append(b)
             adj[b].append(a)
-        def dfs(prev, i):
-            # leaf = True
-            i0,i1 = nums[i],nums[i]^k
-            res = [i0,-inf]
-            first = True
+        def dfs(prev,i):
+            # dpa: i not xor k, dpb: i xor k
+            dpa,dpb = nums[i],-inf
             for ne in adj[i]:
                 if ne != prev:
-                    score0, score1 = dfs(i, ne)
-                    ne0,ne1 = nums[ne],nums[ne]^k
-                    if first:
-                        res[0] = max(i0+score0, i0+score1)
-                        res[1] = max(i1+score0+ne1-ne0, i1+score1+ne0-ne1)
-                        first = False
-                    else:
-                        res = [max(res[0]+score0, res[0]+score1, res[1]+i0-i1+score0-ne0+ne1, res[1]+i0-i1+score1-ne1+ne0), max(res[1]+score0, res[1]+score1, res[0]+i1-i0+score0-ne0+ne1, res[0]+i1-i0+score1-ne1+ne0)]
-                    # if i == 0:
-                    #     print('---',res,i0,i1,ne0,ne1, res[0]+i1-i0+score0-ne0+ne1, res[0]+i1-i0+score1-ne1+ne0)
-                    # leaf = False
-                    
-            # print(i, res)
-            return res
+                    # a: ne not xor k, b: ne xor k
+                    a,b = dfs(i,ne)
+                    # not xor edge
+                    n_dpa = max(a,b) + dpa
+                    n_dpb = max(a,b) + dpb
+                    # xor edge
+                    n_dpa = max(n_dpa, 
+                                dpb-(nums[i]^k)+nums[i] + a-nums[ne]+(nums[ne]^k), 
+                                dpb-(nums[i]^k)+nums[i] + b-(nums[ne]^k)+nums[ne])
+                    n_dpb = max(n_dpb, 
+                                dpa+(nums[i]^k)-nums[i] + a-nums[ne]+(nums[ne]^k), 
+                                dpa+(nums[i]^k)-nums[i] + b-(nums[ne]^k)+nums[ne])
+                    dpa,dpb = n_dpa,n_dpb
+            if len(adj[i]) == 1 and prev != -1:
+                return nums[i],-inf
+            return dpa,dpb
         return max(dfs(-1,0))
