a8b2602d6d69a565cd4000c35efc42259228a469
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2024-05-18 17:54:48 -0700

    Time: 961 ms (59.09%), Space: 28.3 MB (25.57%) - LeetHub

--- 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.py
+++ 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.py
@@ -1,5 +1,6 @@
 class Solution:
     def maximumValueSum(self, nums: List[int], k: int, edges: List[List[int]]) -> int:
+        # tree -> not cycle -> only 1 path between 2 nodes -> can replace any 2 nodes with their xor values
         dpa,dpb = 0,-inf
         for n in nums:
             n_dpa = max(dpa+n, dpb+(n^k))
