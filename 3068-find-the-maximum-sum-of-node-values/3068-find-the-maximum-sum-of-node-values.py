eabdc92fafb55eb73bdf89439baad3c2030fb2bd
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2024-05-18 17:53:47 -0700

    Time: 958 ms (67.61%), Space: 28.4 MB (22.16%) - LeetHub

--- 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.py
+++ 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.py
@@ -1,28 +1,8 @@
 class Solution:
     def maximumValueSum(self, nums: List[int], k: int, edges: List[List[int]]) -> int:
-        adj = defaultdict(list)
-        for a,b in edges:
-            adj[a].append(b)
-            adj[b].append(a)
-        def dfs(prev,i):
-            if len(adj[i]) == 1 and prev != -1:
-                return nums[i],-inf
-            # dpa: i not xor k, dpb: i xor k
-            dpa,dpb = nums[i],-inf
-            for ne in adj[i]:
-                if ne != prev:
-                    # a: ne not xor k, b: ne xor k
-                    a,b = dfs(i,ne)
-                    # not xor edge
-                    n_dpa = max(a,b) + dpa
-                    n_dpb = max(a,b) + dpb
-                    # xor edge
-                    n_dpa = max(n_dpa, 
-                                dpb-(nums[i]^k)+nums[i] + a-nums[ne]+(nums[ne]^k), 
-                                dpb-(nums[i]^k)+nums[i] + b-(nums[ne]^k)+nums[ne])
-                    n_dpb = max(n_dpb, 
-                                dpa+(nums[i]^k)-nums[i] + a-nums[ne]+(nums[ne]^k), 
-                                dpa+(nums[i]^k)-nums[i] + b-(nums[ne]^k)+nums[ne])
-                    dpa,dpb = n_dpa,n_dpb
-            return dpa,dpb
-        return max(dfs(-1,0))
+        dpa,dpb = 0,-inf
+        for n in nums:
+            n_dpa = max(dpa+n, dpb+(n^k))
+            n_dpb = max(dpb+n, dpa+(n^k))
+            dpa,dpb = n_dpa,n_dpb
+        return dpa
