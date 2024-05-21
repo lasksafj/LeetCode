fe42a1a976e46daaf2406529773c39e16fb7ff2f
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2024-05-20 23:00:53 -0700

    Time: 2164 ms (14.12%), Space: 51.3 MB (61.26%) - LeetHub

--- 3123-find-edges-in-shortest-paths/3123-find-edges-in-shortest-paths.py
+++ 3123-find-edges-in-shortest-paths/3123-find-edges-in-shortest-paths.py
@@ -6,7 +6,7 @@ def F(adj,start,n):
         d,cur = heappop(pq)
         if res[cur] < d:
             continue
-        for ne,w,_ in adj[cur]:
+        for ne,w in adj[cur]:
             if res[ne] > d+w:
                 res[ne] = d+w
                 heappush(pq, [d+w,ne])
@@ -15,16 +15,12 @@ def F(adj,start,n):
 class Solution:
     def findAnswer(self, n: int, edges: List[List[int]]) -> List[bool]:
         adj = defaultdict(list)
-        for i,(a,b,w) in enumerate(edges):
-            adj[a].append([b,w,i])
-            adj[b].append([a,w,i])
+        for a,b,w in edges:
+            adj[a].append([b,w])
+            adj[b].append([a,w])
         dist1 = F(adj,0,n)
         dist2 = F(adj,n-1,n)
         mi = dist1[n-1]
-        
-        # print(mi)
-        # print(dist1)
-        # print(dist2)
         
         res = [False]*len(edges)
         if mi == inf:
