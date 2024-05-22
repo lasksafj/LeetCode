cc20de009c5b50c436eefef0f043d59026b014bc
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2024-05-21 23:45:54 -0700

    Time: 288 ms (90.56%), Space: 19.1 MB (20.39%) - LeetHub

--- 3081-replace-question-marks-in-string-to-minimize-its-value/3081-replace-question-marks-in-string-to-minimize-its-value.py
+++ 3081-replace-question-marks-in-string-to-minimize-its-value/3081-replace-question-marks-in-string-to-minimize-its-value.py
@@ -1,35 +1,23 @@
 from sortedcontainers import SortedList
 class Solution:
     def minimizeStringValue(self, s: str) -> str:
-        L = [0]*26
-        R = [[0]*26 for _ in range(len(s))]
-        for ch in ascii_lowercase:
-            for i in range(len(s)-1,0,-1):
-                R[i-1][ord(ch)-ord('a')] = R[i][ord(ch)-ord('a')] + (s[i] == ch)
-        i = 0
-        res = []
-        
+        cnt = Counter(s)
+        k = cnt['?']
+        pq = [[cnt[ch],ch] for ch in ascii_lowercase]
+        heapify(pq)
         A = []
-        while i < len(s):
-            if s[i] == '?':
-                mi = inf
-                mi_idx = -1
-                for j in range(26):
-                    if L[j] + R[i][j] < mi:
-                        mi = L[j] + R[i][j]
-                        mi_idx = j
-                L[mi_idx] += 1
-                A.append(mi_idx)
-            else:
-                L[ord(s[i])-ord('a')] += 1
-            i += 1
+        for _ in range(k):
+            d,ch = heappop(pq)
+            A.append(ch)
+            d += 1
+            heappush(pq, [d,ch])
         A.sort()
+        res = []
         i = 0
         for ch in s:
             if ch == '?':
-                res.append(chr(A[i]+ord('a')))
+                res.append(A[i])
                 i += 1
             else:
                 res.append(ch)
-        
         return ''.join(res)
