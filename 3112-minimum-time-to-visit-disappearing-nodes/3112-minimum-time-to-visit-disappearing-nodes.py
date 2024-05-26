22cad94feeff04f4a82132f95f9473980b9ab477
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2024-05-25 19:23:46 -0700

    Time: 1885 ms (13.95%), Space: 75.9 MB (12.81%) - LeetHub

--- 3112-minimum-time-to-visit-disappearing-nodes/3112-minimum-time-to-visit-disappearing-nodes.py
+++ 3112-minimum-time-to-visit-disappearing-nodes/3112-minimum-time-to-visit-disappearing-nodes.py
@@ -1,6 +1,5 @@
 class Solution:
     def minimumTime(self, n: int, edges: List[List[int]], disappear: List[int]) -> List[int]:
-        P = sorted(list(range(n)), key=lambda i:disappear[i])
         adj = defaultdict(list)
         for a,b,w in edges:
             adj[a].append((b,w))
@@ -8,21 +7,12 @@ class Solution:
         pq = [[0,0]]
         vis = [inf]*n
         vis[0] = 0
-        j = 0
         while pq:
             t,cur = heappop(pq)
             if t > vis[cur]:
                 continue
-            while j < len(P) and t >= disappear[P[j]]:
-                if P[j] in adj:
-                    del adj[P[j]]
-                j += 1
             for ne,w in adj[cur]:
-                if ne not in adj:
-                    continue
-                if t+w >= disappear[ne]:
-                    continue
-                if t+w < vis[ne]:
+                if t+w < vis[ne] and t+w < disappear[ne]:
                     vis[ne] = t+w
                     heappush(pq, [t+w, ne])
         return [v if v < inf else -1 for v in vis]
