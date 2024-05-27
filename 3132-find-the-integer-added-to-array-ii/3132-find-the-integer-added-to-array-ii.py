b95bbecfd2c210514999ce7328f094b7007f07b7
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2024-05-27 16:55:04 -0700

    Time: 42 ms (97.20%), Space: 16.5 MB (85.10%) - LeetHub

--- 3132-find-the-integer-added-to-array-ii/3132-find-the-integer-added-to-array-ii.py
+++ 3132-find-the-integer-added-to-array-ii/3132-find-the-integer-added-to-array-ii.py
@@ -4,21 +4,19 @@ class Solution:
         nums2.sort()
         s1 = sum(nums1)
         s2 = sum(nums2)
-        for i in range(len(nums1)):
-            for j in range(i):
-                a,b = nums1[i],nums1[j]
-                d = s1-(a+b)-s2
-                if d%(len(nums2)) == 0:
-                    x = -d // len(nums2)
-                    k2 = 0
-                    ok = True
-                    for k1 in range(len(nums1)):
-                        if k1 == i or k1 == j:
-                            continue
-                        if nums1[k1]+x == nums2[k2]:
-                            k2 += 1
-                        else:
-                            ok = False
-                    if ok:
-                        return x
-        return -1
+        def check(start, skip, x):
+            j = start
+            for i in range(len(nums2)):
+                while nums1[j] + x != nums2[i]:
+                    skip -= 1
+                    if skip < 0:
+                        return False
+                    j += 1
+                j += 1
+            return True
+        
+        if check(2, 0, nums2[0] - nums1[2]):
+            return nums2[0] - nums1[2]
+        if check(1, 1, nums2[0] - nums1[1]):
+            return nums2[0] - nums1[1]
+        return nums2[0] - nums1[0]
