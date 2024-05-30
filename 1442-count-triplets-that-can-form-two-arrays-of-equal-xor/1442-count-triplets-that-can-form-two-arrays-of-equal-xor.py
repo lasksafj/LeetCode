9688de97b1c216851529f6b7678ec800b2221caa
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2024-05-30 00:23:25 -0700

    Time: 32 ms (97.13%), Space: 16.5 MB (66.09%) - LeetHub

--- 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.py
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.py
@@ -1,13 +1,22 @@
 class Solution:
     def countTriplets(self, arr: List[int]) -> int:
+        # a = arr[i] ^ arr[i + 1] ^ ... ^ arr[j - 1]
+        # b = arr[j] ^ arr[j + 1] ^ ... ^ arr[k]
+        # a == b => a ^ b = 0
+        # => arr[i] ^ arr[i + 1] ^ ... ^ arr[j - 1] ^ arr[j] ^ arr[j + 1] ^ ... ^ arr[k] = 0
+        # => xor(arr[i:k+1]) = 0 for all j, i < j <= k
+        # => number of triplet = k-i-1
+        N = len(arr)
+        prexor = [0]*(N+1)
+        for i,n in enumerate(arr):
+            prexor[i+1] = prexor[i] ^ arr[i]
         res = 0
-        for k in range(len(arr)):
-            mp = defaultdict(int)
-            l,r = 0,0
-            for i in range(k-1,-1,-1):
-                l ^= arr[i]
-                mp[l] += 1
-            for i in range(k, len(arr)):
-                r ^= arr[i]
-                res += mp[r]
+        cnt,total_no_idx = defaultdict(int),defaultdict(int)
+        cnt[0] = 1
+        cur = 0
+        for i in range(N):
+            cur ^= arr[i]
+            res += i * cnt[cur] - total_no_idx[cur]
+            cnt[cur] += 1
+            total_no_idx[cur] += i+1
         return res
