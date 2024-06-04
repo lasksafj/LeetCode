3fd2c94565de11b4c84a225d266ce6394213d472
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2024-06-03 21:07:46 -0700

    Time: 42 ms (29.06%), Space: 16.3 MB (99.59%) - LeetHub

--- 3133-minimum-array-end/3133-minimum-array-end.py
+++ 3133-minimum-array-end/3133-minimum-array-end.py
@@ -1,18 +1,12 @@
 class Solution:
     def minEnd(self, n: int, x: int) -> int:
-        n = bin(n-1)[2:][::-1]
-        x = bin(x)[2:][::-1]
-        res = []
-        i,j = 0,0
-        while i < len(x) or j < len(n):
-            if j < len(n) and (i >= len(x) or x[i] == '0'):
-                res.append(n[j] == '1')
-                j += 1
-            else:
-                res.append(x[i] == '1')
-            i += 1
-        ans = 0
-        for i in res[::-1]:
-            ans <<= 1
-            ans |= i
-        return ans
+        a = 1
+        n -= 1
+        res = x
+        while n:
+            if a&x == 0:
+                if n&1:
+                    res |= a 
+                n >>= 1
+            a <<= 1
+        return res
