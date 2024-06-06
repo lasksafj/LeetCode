d0a399b2ad52840e703df2968ec1ac91b520c864
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2024-06-05 18:08:24 -0700

    Time: 739 ms (41.04%), Space: 39.9 MB (76.87%) - LeetHub

--- 1713-minimum-operations-to-make-a-subsequence/1713-minimum-operations-to-make-a-subsequence.py
+++ 1713-minimum-operations-to-make-a-subsequence/1713-minimum-operations-to-make-a-subsequence.py
@@ -1,15 +1,15 @@
 class Solution:
     def minOperations(self, target: List[int], arr: List[int]) -> int:
-        from sortedcontainers import SortedList
         mp = {}
         for i,t in enumerate(target):
             mp[t] = i
-        A = SortedList()
+        A = []
         for a in arr:
             if a in mp:
                 b = mp[a]
-                p = A.bisect_left(b)
-                if p < len(A):
-                    del A[p]
-                A.add(b)
+                p = bisect_left(A, b)
+                if p == len(A):
+                    A.append(b)
+                else:
+                    A[p] = b
         return max(0, len(target) - len(A))
