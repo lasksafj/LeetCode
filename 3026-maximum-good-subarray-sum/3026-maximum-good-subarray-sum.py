26e223a182effe00ccc2dbd951e0efa8bdfc3e7b
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2024-06-13 18:45:58 -0700

    Time: 859 ms (85.49%), Space: 30.4 MB (81.70%) - LeetHub

--- 3026-maximum-good-subarray-sum/3026-maximum-good-subarray-sum.py
+++ 3026-maximum-good-subarray-sum/3026-maximum-good-subarray-sum.py
@@ -1,16 +1,16 @@
 class Solution:
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
-        L = {}
-        sum_left = 0
-        sum_cur = 0
         res = -inf
-        for i,x in enumerate(nums):
-            sum_cur += x
-            if x-k in L:
-                res = max(res, sum_cur - L[x-k])
-            if x+k in L:
-                res = max(res, sum_cur - L[x+k])
-            if x not in L or L[x] > sum_left:
-                L[x] = sum_left
-            sum_left += x
+        cur = 0
+        mp = {}
+        for n in nums:
+            cur += n
+            if n-k in mp:
+                res = max(res, cur - mp[n-k])
+            if n+k in mp:
+                res = max(res, cur - mp[n+k])
+            if n in mp:
+                mp[n] = min(mp[n], cur-n)
+            else:
+                mp[n] = cur-n
         return res if res > -inf else 0
