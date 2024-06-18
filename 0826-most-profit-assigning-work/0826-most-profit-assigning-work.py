07b1bb8219e6b90050ea052878467789f152091c
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2024-06-18 00:41:14 -0700

    Time: 326 ms (34.22%), Space: 18.9 MB (99.17%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
@@ -3,12 +3,12 @@ class Solution:
         worker.sort()
         P = sorted(list(range(len(difficulty))), key=lambda i:difficulty[i])
         res = 0
-        pq = []
         i = 0
+        ma = 0
         for w in worker:
             while i < len(difficulty) and difficulty[P[i]] <= w:
-                heappush(pq, -profit[P[i]])
+                ma = max(ma, profit[P[i]])
                 i += 1
-            if pq:
-                res += -pq[0]
+            if ma:
+                res += ma
         return res
