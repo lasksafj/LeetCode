a2a0dd99fe2adb2fd2d3189466051d6cee9a3f4e
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2024-06-21 13:10:27 -0700

    Time: 224 ms (27.66%), Space: 18.8 MB (64.08%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.py
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.py
@@ -1,13 +1,17 @@
 class Solution:
     def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
         N = len(customers)
-        pre = list(accumulate(customers, initial=0))
-        @cache
-        def dfs(i, use):
-            if i >= N:
-                return 0
-            res = dfs(i+1, use) + (customers[i] if grumpy[i] == 0 else 0)
-            if use:
-                res = max(res, dfs(i+minutes, 0) + pre[min(N,i+minutes)]-pre[i])
-            return res
-        return dfs(0,1)
+        # max unsatisfied in minutes
+        ma_unsatisfied = 0
+        unsatisfied = 0
+        for i in range(N):
+            if i < minutes:
+                unsatisfied += customers[i]*grumpy[i]
+                ma_unsatisfied = unsatisfied
+            else:
+                unsatisfied += customers[i]*grumpy[i] - customers[i-minutes]*grumpy[i-minutes]
+                ma_unsatisfied = max(ma_unsatisfied, unsatisfied)
+        res = ma_unsatisfied
+        for c,g in zip(customers, grumpy):
+            res += c*(1-g)
+        return res
