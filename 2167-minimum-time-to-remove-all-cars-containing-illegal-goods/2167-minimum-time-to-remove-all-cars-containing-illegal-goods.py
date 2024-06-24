95a593e78a1c33b98228e88552e6cafdef5df6a9
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2024-06-23 23:23:19 -0700

    Time: 1274 ms (40.68%), Space: 18.8 MB (61.02%) - LeetHub

--- 2167-minimum-time-to-remove-all-cars-containing-illegal-goods/2167-minimum-time-to-remove-all-cars-containing-illegal-goods.py
+++ 2167-minimum-time-to-remove-all-cars-containing-illegal-goods/2167-minimum-time-to-remove-all-cars-containing-illegal-goods.py
@@ -1,23 +1,10 @@
 class Solution:
     def minimumTime(self, s: str) -> int:
         N = len(s)
-        A = []
-        for k in range(N):
-            A.append(int(s[k]=='1'))
-        ss = sum(A)
-        cur = ss
-        mi = cur*2
-        B = [mi]
-        for i in range(1,N):
-            cur -= A[i-1]
-            mi = min(mi, cur*2+i)
-            B.append(mi)
-        cur = ss
-        mi = cur*2
-        res = ss*2
-        for j in range(N-1,-1,-1):
-            cur -= A[j]
-            mi = min(mi, cur*2+N-j)
-            res = min(res, B[j]+mi-ss*2)
-        
+        left = 0
+        res = inf
+        for i in range(N):
+            cur = int(s[i]=='1')
+            left = min(left+cur*2, i+1)
+            res = min(res, left+N-i-1)
         return res
