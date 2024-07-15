f53949c7b013ea498d6ad133e1a23b5a59933e5e
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2024-07-15 10:13:37 -0700

    Time: 1429 ms (17.46%), Space: 54.5 MB (49.21%) - LeetHub

--- 1840-maximum-building-height/1840-maximum-building-height.py
+++ 1840-maximum-building-height/1840-maximum-building-height.py
@@ -1,20 +1,25 @@
 class Solution:
     def maxBuilding(self, n: int, restrictions: List[List[int]]) -> int:
-        if restrictions == []:
-            return n-1
-        A = sorted(restrictions)
-        # reduce each height in A to its highest possible -> result in st
-        res = inf
-        st = [[1,0]]
-        for i,h in A:
-            cur_res = 0
-            while st and h < st[-1][1] - (i-st[-1][0]):
-                st.pop()
-            if not st or i-st[-1][0] + st[-1][1] >= h:
-                st.append([i,h])
+        restrictions.sort()
+        # reduce each height in A to its highest possible -> result in A
+        A = [[1,0]]
+        for i,h in restrictions:
+            li,lh = A[-1]
+            A.append([i, min(lh+i-li, h)])
+        for k in range(len(A)-2,-1,-1):
+            i,h = A[k]
+            ri,rh = A[k+1]
+            h = min(h, rh+ri-i)
+            A[k][1] = h
         res = 0
-        for k in range(1,len(st)):
-            li,lh = st[k-1]
-            i,h = st[k]
+        # find max height between A[k-1] and A[k]
+        # x: max height
+        # li,lh = A[k-1]
+        # i,h = A[k]
+        # x-lh+x-h=i-li => 2x=h+lh+i-li
+        for k in range(1,len(A)):
+            li,lh = A[k-1]
+            i,h = A[k]
             res = max(res, (h+lh+i-li)//2)
-        return max(res, n-st[-1][0]+st[-1][1])
+        
+        return max(res, A[-1][1] + n-A[-1][0])
