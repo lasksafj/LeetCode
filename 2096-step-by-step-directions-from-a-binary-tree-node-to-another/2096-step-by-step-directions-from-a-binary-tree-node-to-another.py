ec8048699e304fcdcb75617480fcc7d0296a50f4
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2024-07-16 01:07:49 -0700

    Time: 578 ms (40.77%), Space: 50.3 MB (86.58%) - LeetHub

--- 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
@@ -6,46 +6,22 @@
 #         self.right = right
 class Solution:
     def getDirections(self, root: Optional[TreeNode], startValue: int, destValue: int) -> str:
-        ans = ''
-        def dfs(r):
-            nonlocal ans
-            res = ['',0]
-            if not r:
-                return res
+        def lca(r):
+            if not r or r.val == startValue or r.val == destValue:
+                return r
+            left,right = lca(r.left),lca(r.right)
+            return r if left and right else left or right
+        r = lca(root)
+        sp,dp = '', ''
+        q = deque([[r, '']])
+        while q and (sp == '' or dp == ''):
+            r,p = q.popleft()
             if r.val == startValue:
-                res = ['', 1]
-            elif r.val == destValue:
-                res = ['', 2]
-            
-            a = dfs(r.left)
-            b = dfs(r.right)
-            
-            
-            if a[1] == 1 and b[1] == 2:
-                ans = a[0]+'U' + 'R'+b[0]
-            elif a[1] == 2 and b[1] == 1:
-                ans = b[0]+'U' + 'L'+a[0]
-            elif a[1] == 1:
-                if res[1] == 2:
-                    ans = a[0]+'U'
-                else:
-                    res = [a[0]+'U', 1]
-            elif a[1] == 2:
-                if res[1] == 1:
-                    ans = 'L'+a[0]
-                else:
-                    res = ['L'+a[0], 2]
-            elif b[1] == 1:
-                if res[1] == 2:
-                    ans = b[0]+'U'
-                else:
-                    res = [b[0]+'U', 1]
-            elif b[1] == 2:
-                if res[1] == 1:
-                    ans = 'R'+b[0]
-                else:
-                    res = ['R'+b[0], 2]
-            # print(r.val,a,b,res)
-            return res
-        dfs(root)
-        return ans
+                sp = p
+            if r.val == destValue:
+                dp = p
+            if r.left:
+                q.append([r.left, p+'L'])
+            if r.right:
+                q.append([r.right, p+'R'])
+        return 'U'*len(sp) + dp
