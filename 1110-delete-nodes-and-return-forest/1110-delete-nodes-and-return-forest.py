d6132f92709ba439e7ccc890ae5b8652459c5828
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2024-07-16 21:04:22 -0700

    Time: 44 ms (97.30%), Space: 17 MB (24.30%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.py
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.py
@@ -10,20 +10,17 @@ class Solution:
         res = []
         if root.val not in to_delete:
             res = [root]
-        def dfs(r, f):
+        def dfs(r):
             if not r:
                 return None
-            dfs(r.left, r)
-            dfs(r.right, r)
+            r.left = dfs(r.left)
+            r.right = dfs(r.right)
             if r.val in to_delete:
-                if f.left == r:
-                    f.left = None
-                else:
-                    f.right = None
                 if r.left:
                     res.append(r.left)
                 if r.right:
                     res.append(r.right)
+                return None
             return r
-        dfs(root, TreeNode(-1))
+        dfs(root)
         return res
