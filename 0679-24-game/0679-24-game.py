0aaea01e782c78293bfdc535cf1bbeaa60fa0fae
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2024-08-06 19:21:08 -0700

    Time: 68 ms (64.25%), Space: 17.3 MB (7.25%) - LeetHub

--- 0679-24-game/0679-24-game.py
+++ 0679-24-game/0679-24-game.py
@@ -1,11 +1,12 @@
 class Solution:
     def judgePoint24(self, cards: List[int]) -> bool:
+        @cache
         def dfs(A):
             if len(A) == 1:
                 return math.isclose(A[0], 24)
             for a,b,*rest in itertools.permutations(A):
                 for x in {a+b, a-b, a*b, b and a/b}:
-                    if dfs([x] + rest):
+                    if dfs(tuple([x] + rest)):
                         return True
             return False
-        return dfs(cards)
+        return dfs(tuple(cards))
