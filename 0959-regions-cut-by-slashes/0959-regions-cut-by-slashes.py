d713254c8a6ce9901dd8b4493f97e949d070abb3
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2024-08-09 23:38:04 -0700

    Time: 186 ms (30.04%), Space: 19.7 MB (5.14%) - LeetHub

--- 0959-regions-cut-by-slashes/0959-regions-cut-by-slashes.py
+++ 0959-regions-cut-by-slashes/0959-regions-cut-by-slashes.py
@@ -1,17 +1,21 @@
 class Solution:
     def regionsBySlashes(self, grid: List[str]) -> int:
-        n = len(grid)+1
-        
-        f = list(range(n*n))
-        r = [0]*(n*n)
+        # Split a cell in to 4 parts.
+        # We give it a number top is 0, left is 1, bottom is 2, right is 3.
+
+        n = len(grid)
+        f = {}
+        r = defaultdict(int)
         def root(x):
+            if x not in f:
+                f[x] = x
             if x == f[x]:
                 return x
             return root(f[x])
         def union(a,b):
             a,b = root(a),root(b)
             if a == b:
-                return 1
+                return 
             if r[a] < r[b]:
                 f[a] = b
             elif r[a] > r[b]:
@@ -19,22 +23,21 @@ class Solution:
             else:
                 f[a] = b
                 r[b] += 1
-            return 0
         
         for i in range(n):
             for j in range(n):
-                if i==0 or i==n-1 or j==0 or j==n-1:
-                    d = i*n+j
-                    union(d, 0)
-        res = 1
-        for i in range(n-1):
-            for j in range(n-1):
+                if i:
+                    union((i,j,0), (i-1,j,2))
+                if j:
+                    union((i,j,1), (i,j-1,3))
                 if grid[i][j] == '/':
-                    d1 = i*n+j+1
-                    d2 = (i+1)*n+j
-                    res += union(d1, d2)
+                    union((i,j,0), (i,j,1))
+                    union((i,j,2), (i,j,3))
                 elif grid[i][j] == '\\':
-                    d1 = i*n+j
-                    d2 = (i+1)*n+j+1
-                    res += union(d1, d2)
-        return res
+                    union((i,j,0), (i,j,3))
+                    union((i,j,1), (i,j,2))
+                else:
+                    union((i,j,0), (i,j,1))
+                    union((i,j,1), (i,j,2))
+                    union((i,j,2), (i,j,3))
+        return len(set(root(x) for x in f))
