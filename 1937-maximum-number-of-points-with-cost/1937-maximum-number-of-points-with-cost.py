60e9ad4328b6a346e2ca804081ac310f5c5fd365
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2024-08-16 18:19:09 -0700

    Time: 1451 ms (92.68%), Space: 50.1 MB (93.17%) - LeetHub

--- 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.py
+++ 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.py
@@ -4,26 +4,14 @@ class Solution:
         dp = points[0]
         for row in points[1:]:
             ndp = [0]*N
-            A = []
-            ma,ma_i = 0,0
-            for i in range(N):
-                cur = ma-abs(i-ma_i)
-                if dp[i] > cur:
-                    ma = dp[i]
-                    ma_i = i
-                    cur = ma
-                A.append([cur,ma_i])
-            ma,ma_i = 0,0
+            A = [dp[0]]
+            for i in range(1,N):
+                A.append(max(A[i-1]-1, dp[i]))
+            A.append(0)
             for i in range(N-1,-1,-1):
-                cur = ma-abs(i-ma_i)
-                if dp[i] > cur:
-                    ma = dp[i]
-                    ma_i = i
-                    cur = ma
-                if cur > A[i][0]:
-                    A[i] = [cur,ma_i]
+                A[i] = max(A[i], A[i+1]-1)
             for i,p in enumerate(row):
-                ndp[i] = A[i][0] + p
+                ndp[i] = A[i] + p
             
             dp = ndp
         return max(dp)
