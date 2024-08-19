100e28062ea4a511319d96f9b97c3d35ecbcbba3
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2024-08-19 09:52:53 -0700

    Time: 18872 ms (5.08%), Space: 18.6 MB (22.57%) - LeetHub

--- 3234-count-the-number-of-substrings-with-dominant-ones/3234-count-the-number-of-substrings-with-dominant-ones.py
+++ 3234-count-the-number-of-substrings-with-dominant-ones/3234-count-the-number-of-substrings-with-dominant-ones.py
@@ -1,37 +1,19 @@
+fmin = lambda x, y: x if x < y else y
+fmax = lambda x, y: x if x > y else y
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-        A = [-1]
-        for i,ch in enumerate(s):
-            if ch == '0':
-                A.append(i)
-        A.append(len(s)-1)
-        def F(x):
-            i = 0
-            res = 0
-            no0 = 0
-            l = 0
-            for r in range(len(s)):
-                no0 += s[r]=='0'
-                if no0 > x:
-                    i += 1
-                    l = A[i]+1
-                    no0 -= 1
-                if no0 == x:
-                    a = no0
-                    while a==x and l<=r and l <= A[i+1] and a**2 <= r-l+1 - a:
-                        a -= s[l]=='0'
-                        l += 1
-                    if i < len(A) and l-1 <= r and A[i]<l:
-                        res += l-A[i]-1
-                        # print('--',l,r,A[i],'--',res)
-
-
-            # print(x,res)
-            return res
+        N = len(s)
+        A = [-1] + [i for i in range(N) if s[i] == '0'] + [N]
         res = 0
-        x = 0
-        while x**2+x <= len(s):
-            res += F(x)
-            x += 1
-        # print()
+        j = 0
+        ma = isqrt(N)+1
+        for i,ch in enumerate(s):
+            for z in range(ma):
+                if j+z >= len(A)-1:
+                    break
+                r = fmax(i,A[j+z])
+                a = fmax(0, z*z+z - (r-i+1))
+                res += fmax(0, A[j+z+1] - fmax(i, A[j+z]) - a)
+            if ch == '0':
+                j += 1
         return res
