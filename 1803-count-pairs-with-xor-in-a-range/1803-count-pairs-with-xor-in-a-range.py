0a99a978eccca33567c6ce1373b75478913a6011
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2024-08-22 13:19:45 -0700

    Time: 6736 ms (6.52%), Space: 28.4 MB (34.78%) - LeetHub

---
+++ 1803-count-pairs-with-xor-in-a-range/1803-count-pairs-with-xor-in-a-range.py
@@ -0,0 +1,43 @@
+class Solution:
+    def countPairs(self, nums: List[int], low: int, high: int) -> int:
+        def add(n,T):
+            cur = T
+            n |= 1<<32
+            for ch in bin(n)[3:]:
+                ch = int(ch)
+                if ch not in cur:
+                    cur[ch] = {}
+                cur = cur[ch]
+                cur['cnt'] = cur.get('cnt', 0) + 1
+        
+        def calc(n, k, T):
+            n |= 1<<32
+            k |= 1<<32
+            k = bin(k)[3:]
+            j = 0
+            cur = T
+            res = 0
+            
+            for ch in bin(n)[3:]:
+                ch = int(ch)
+                if k[j] == '0':
+                    if ch not in cur:
+                        return res
+                    cur = cur[ch]
+                else:
+                    if ch in cur:
+                        res += cur[ch].get('cnt', 0)
+                    ne = ch^1
+                    if ne not in cur:
+                        return res
+                    cur = cur[ne]
+                j += 1
+            return res
+        
+        def sol(k, T):
+            res = 0
+            for n in nums:
+                res += calc(n, k, T)
+                add(n, T)
+            return res
+        return sol(high+1, {}) - sol(low, {})
