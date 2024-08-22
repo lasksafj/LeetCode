46c8a94675dc6e7c31e512dfa7eed2b721b49f89
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2024-08-22 16:51:42 -0700

    Time: 935 ms (21.18%), Space: 26 MB (13.84%) - LeetHub

--- 0396-rotate-function/0396-rotate-function.py
+++ 0396-rotate-function/0396-rotate-function.py
@@ -1,16 +1,14 @@
 class Solution:
     def maxRotateFunction(self, nums: List[int]) -> int:
         N = len(nums)
-        A = nums+nums
-        s =  sum(nums)
+        s = sum(nums)
         cur = 0
         for i in range(N):
-            cur += A[i]*i
+            cur += nums[i]*i
         res = cur
         j = 0
         for i in range(N, 2*N):
-            cur += A[i]*i - A[j]*j - s
-            # print(i,cur,s)
+            cur += nums[i%N]*i - nums[j]*j - s
             res = max(res, cur)
             j += 1
         return res
