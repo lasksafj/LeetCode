b9fedb070f9f391509c789518862f848cad5842d
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2025-05-04 18:27:36 -0700

    Time: 1 ms (66.81%), Space: 18.1 MB (30.56%) - LeetHub

--- 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.py
+++ 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.py
@@ -1,10 +1,12 @@
 class Solution:
     def numTilings(self, n: int) -> int:
+        # dp[n]=dp[n-1]+dp[n-2]+ 2*(dp[n-3]+...+d[0])
+        # = dp[n-1]+dp[n-2]+dp[n-3]+dp[n-3]+2*(dp[n-4]+...+d[0])
+        # = dp[n-1]+dp[n-3]+(dp[n-2]+dp[n-3]+2*(dp[n-4]+...+d[0]))
+        # = dp[n-1]+dp[n-3]+dp[n-1]
+        # = 2*dp[n-1]+dp[n-3]
         MOD = 10**9+7
-        dp = [[[0]*2 for _ in range(2)] for _ in range(n+2)]
-        dp[1][1][1] = 1
-        for i in range(2, n+2):
-            dp[i][1][1] = dp[i-1][1][1] + dp[i-1][0][1] + dp[i-1][1][0] + dp[i-2][1][1]
-            dp[i][0][1] = dp[i-2][1][1] + dp[i-1][1][0]
-            dp[i][1][0] = dp[i-2][1][1] + dp[i-1][0][1]
-        return dp[-1][1][1] % MOD
+        dp = [1, 2, 5] + [0] * (n-3)
+        for i in range(3, n):
+            dp[i] = 2*dp[i-1] + dp[i-3]
+        return dp[n-1] % MOD
