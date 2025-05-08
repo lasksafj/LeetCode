d372e4f2bf0094312283407781bbb8d92384184b
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2025-05-07 21:45:19 -0700

    Time: 1334 ms (92.7%), Space: 83.4 MB (95.71%) - LeetHub

--- 3628-find-minimum-time-to-reach-last-room-ii/3628-find-minimum-time-to-reach-last-room-ii.py
+++ 3628-find-minimum-time-to-reach-last-room-ii/3628-find-minimum-time-to-reach-last-room-ii.py
@@ -2,14 +2,16 @@ class Solution:
     def minTimeToReach(self, moveTime: List[List[int]]) -> int:
         M,N = len(moveTime), len(moveTime[0])
         pq = [[0, 0,0,1]]
-        vis = [[inf]*N for _ in range(M)]
+        vis = [[False]*N for _ in range(M)]
+        vis[0][0] = True
         while pq:
             t,i,j,a = heappop(pq)
+            if (i,j) == (M-1,N-1):
+                return t
             for ni,nj in [[i+1,j],[i-1,j],[i,j+1],[i,j-1]]:
-                if 0<=ni<M and 0<=nj<N:
+                if 0<=ni<M and 0<=nj<N and not vis[ni][nj]:
                     nt = max(t, moveTime[ni][nj]) + a
                     na = 3-a
-                    if vis[ni][nj] > nt:
-                        vis[ni][nj] = nt
-                        heappush(pq, [nt,ni,nj,na])
+                    vis[ni][nj] = True
+                    heappush(pq, [nt,ni,nj,na])
         return vis[M-1][N-1]
