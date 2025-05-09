6fa507962f62399636b5f849e4991670fb12d107
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2025-05-09 10:59:52 -0700

    Time: 2517 ms (93.62%), Space: 19.6 MB (98.76%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.py
+++ 0312-burst-balloons/0312-burst-balloons.py
@@ -1,14 +1,12 @@
 class Solution:
     def maxCoins(self, nums: List[int]) -> int:
-        # dp[i][j]: max point for (i,j)
-        dp = defaultdict(lambda:defaultdict(int))
-        for l in range(3, len(nums)+3):
-            for i in range(-1,len(nums)):
-                j = i+l-1
-                if j > len(nums):
-                    continue
-                for k in range(i+1,j):
-                    a = nums[i] if i >= 0 else 1
-                    b = nums[j] if j < len(nums) else 1
-                    dp[i][j] = max(dp[i][j], dp[i][k] + dp[k][j] + a*nums[k]*b)
-        return dp[-1][len(nums)]
+        # dp[i][j]: max for (i,j) (not inclusive)
+        # dp[i][j] = dp[i][k] + dp[k][j] + nums[i]*nums[k]*nums[j]
+        nums = [1] + nums + [1]
+        N = len(nums)
+        dp = [[0]*N for _ in range(N)]
+        for j in range(N):
+            for i in range(j-2, -1, -1):
+                for k in range(i+1, j):
+                    dp[i][j] = max(dp[i][j], dp[i][k] + dp[k][j] + nums[i]*nums[k]*nums[j])
+        return dp[0][N-1]
