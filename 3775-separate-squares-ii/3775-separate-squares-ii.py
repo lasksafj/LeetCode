1f5481962746def902cec330c05877fe26cbd7a1
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2025-05-16 13:01:35 -0700

    Time: 2937 ms (66.67%), Space: 107.4 MB (11.11%) - LeetHub

--- 3775-separate-squares-ii/3775-separate-squares-ii.py
+++ 3775-separate-squares-ii/3775-separate-squares-ii.py
@@ -1,33 +1,54 @@
 class SegTree:
-    def __init__(self, nums):
+    def __init__(self, tl, tr, nums):
         self.nums = nums
-        self.N = len(nums)
-        self.cnt = [0]*(self.N*4)
-        self.cover = [0]*(self.N*4)
+        self.tl = tl
+        self.tr = tr
+        self.cover = 0
+        self.min_val = 0
+        self.lazy = 0
+        if tl < tr-1:
+            mi = (tl+tr)//2
+            self.left = SegTree(tl, mi, nums)
+            self.right = SegTree(mi, tr, nums)
+    
+    def merge(self):
+        if self.tl < self.tr-1:
+            if self.min_val:
+                self.cover = self.nums[self.tr] - self.nums[self.tl]
+            else:
+                self.cover = self.left.cover + self.right.cover
+        elif self.tl == self.tr-1:
+            self.cover = self.nums[self.tr] - self.nums[self.tl] if self.min_val else 0
+    
+    def push(self):
+        if self.lazy == 0:
+            return
+        self.min_val += self.lazy
+        if self.tl < self.tr-1:
+            self.left.lazy += self.lazy
+            self.right.lazy += self.lazy
+        self.lazy = 0
 
     def update(self, l, r, inc):
-        self.update_(l, r, inc, 1, 0, self.N-1)
-    
-    def update_(self, l, r, inc, t, tl, tr):
-        if r <= tl or tr <= l:
+        self.push()
+        if r <= self.tl or self.tr <= l:
             return
-        if l <= tl and tr <= r:
-            self.cnt[t] += inc
+        if l <= self.tl and self.tr <= r:
+            self.min_val += inc
+            self.push()
         else:
-            mi = (tl+tr)//2
-            self.update_(l, r, inc, t*2, tl, mi)
-            self.update_(l, r, inc, t*2+1, mi, tr)
-        
-        if tl+1 == tr:
-            self.cover[t] = self.nums[tr] - self.nums[tl] if self.cnt[t] else 0
-        else:
-            if self.cnt[t]:
-                self.cover[t] = self.nums[tr] - self.nums[tl]
-            else:
-                self.cover[t] = self.cover[t*2] + self.cover[t*2+1]
+            # no need check self.tl < self.tr-1, b/c queries have at least 2 points
+            self.left.update(l,r,inc)
+            self.right.update(l,r,inc)
+        self.merge()
 
-    def query(self):
-        return self.cover[1]
+    def query(self, l, r):
+        self.push()
+        if r <= self.tl or self.tr <= l:
+            return 0
+        if l <= self.tl and self.tr <= r:
+            return self.cover
+        return self.left.query(l,r) + self.right.query(l,r)
 
 class Solution:
     def separateSquares(self, squares: List[List[int]]) -> float:
@@ -48,9 +69,9 @@ class Solution:
         total = 0
         tally = []
         py = 0
-        active = SegTree(x_coor)
+        active = SegTree(0, len(x_coor)-1,x_coor)
         for y,x1,x2,typ in events:
-            total += active.query() * (y - py)
+            total += active.query(0, len(x_coor)-1) * (y - py)
             tally.append((y, total))
             
             l = mp[x1]
