45f565b93ee69ed3be232e7a4fec5ce3f3c90589
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2025-05-25 12:30:27 -0700

    Time: 3 ms (85.88%), Space: 18.8 MB (10.59%) - LeetHub

--- 0517-super-washing-machines/0517-super-washing-machines.py
+++ 0517-super-washing-machines/0517-super-washing-machines.py
@@ -4,13 +4,9 @@ class Solution:
         N = len(machines)
         if s%N:
             return -1
-        target = s//N
-        # cur: no clothes needed for [:i]
-        # if cur > 0: need cur time to get cur clothes from [i+1:]
-        # if cur < 0: need cur time to give cur clothes for [i+1:]
-        cur = 0
-        res = 0
+        t = s//N
+        res = cur = 0
         for m in machines:
-            cur = cur + m - target
-            res = max(res, m-target, abs(cur))
+            cur += m-t
+            res = max(res, m-t, abs(cur))
         return res
