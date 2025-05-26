08a892c79ecb37cbd16d77c5f0e63f9facaec652
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2025-05-25 19:26:27 -0700

    Time: 1851 ms (72.14%), Space: 85.4 MB (63.15%) - LeetHub

--- 1986-largest-color-value-in-a-directed-graph/1986-largest-color-value-in-a-directed-graph.py
+++ 1986-largest-color-value-in-a-directed-graph/1986-largest-color-value-in-a-directed-graph.py
@@ -16,14 +16,13 @@ class Solution:
         res = 0
         n = 0
         while q:
-            for _ in range(len(q)):
-                i = q.popleft()
-                n += 1
-                for ne in adj[i]:
-                    for c in range(26):
-                        dp[ne][c] = max(dp[ne][c], dp[i][c])
-                    ind[ne] -= 1
-                    if ind[ne] == 0:
-                        q.append(ne)
-                        dp[ne][colors[ne]] += 1
+            i = q.popleft()
+            n += 1
+            for ne in adj[i]:
+                for c in range(26):
+                    dp[ne][c] = max(dp[ne][c], dp[i][c])
+                ind[ne] -= 1
+                if ind[ne] == 0:
+                    q.append(ne)
+                    dp[ne][colors[ne]] += 1
         return max(max(r) for r in dp) if n == N else -1
