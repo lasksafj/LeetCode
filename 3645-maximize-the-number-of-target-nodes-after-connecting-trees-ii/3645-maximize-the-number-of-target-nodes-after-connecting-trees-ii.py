d618e3a2eaedf31c624737fd8e1bfe9b1575f1ba
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2025-05-29 01:00:54 -0700

    Time: 504 ms (53.41%), Space: 102.6 MB (51.14%) - LeetHub

--- 3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii/3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii.py
+++ 3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii/3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii.py
@@ -6,26 +6,20 @@ class Solution:
                 adj[a].append(b)
                 adj[b].append(a)
             N = len(adj)
-            dp = [[0]*2 for _ in range(N)]
-            def dfs(i,p):
-                e,o = 0,0
+            color = [0]*N
+            def dfs(i,p, d):
+                color[i] = d
                 for ne in adj[i]:
                     if ne == p: continue
-                    ne, no = dfs(ne, i)
-                    e += no
-                    o += ne
-                dp[i] = [e+1, o]
-                return dp[i]
-            dfs(0, -1)
-            ans = [[0]*2 for _ in range(N)]
-            def dfs2(i,p, e,o):
-                ans[i] = [e,o]
-                for ne in adj[i]:
-                    if ne == p: continue
-                    dfs2(ne, i, o, e)
-            dfs2(0, -1, dp[0][0], dp[0][1])
-            return ans
-        A = f(edges1)
-        B = f(edges2)
-        ma_odd2 = max([o for _,o in B])
-        return [a + ma_odd2 for a,_ in A]
+                    dfs(ne, i, d^1)
+            dfs(0,-1,0)
+            return color
+        color1 = f(edges1)
+        color2 = f(edges2)
+        cnt1 = Counter(color1)
+        cnt2 = Counter(color2)
+        ma = max(cnt2.values())
+        res = [0]*len(color1)
+        for i in range(len(color1)):
+            res[i] = cnt1[color1[i]] + ma
+        return res
