daa1e62da06283842c448ca2cf7d0e36fd9ab0d1
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2025-06-04 15:59:17 -0700

    Time: 3482 ms (55.17%), Space: 34.3 MB (61.21%) - LeetHub

---
+++ 3808-longest-palindrome-after-substring-concatenation-ii/3808-longest-palindrome-after-substring-concatenation-ii.py
@@ -0,0 +1,42 @@
+def p(s):
+    N = len(s)
+    dp = [[0]*N for _ in range(N)]
+    res = [1]*N + [0]
+    for j in range(N):
+        for i in range(j,-1,-1):
+            if s[i] == s[j]:
+                if i == j:
+                    dp[i][j] = 1
+                elif i+1 == j:
+                    dp[i][j] = 2
+                elif dp[i+1][j-1]:
+                    dp[i][j] = dp[i+1][j-1] + 2
+            res[i] = max(res[i], dp[i][j])
+    return res
+
+class Solution:
+    def longestPalindrome(self, s: str, t: str) -> int:
+        t = t[::-1]
+        ps = p(s)
+        pt = p(t)
+        
+        n1,n2 = len(s), len(t)
+        res = 0
+        for k in range(-n1, n2):
+            a = max(0, -k-1)
+            b = max(0, k)
+            cur = 0
+            while a < n1 and b < n2:
+                if s[a] == t[b]:
+                    cur += 1
+                else:
+                    res = max(res, cur*2 + max(ps[a], pt[b]))
+                    cur = 0
+                a += 1
+                b += 1
+            if cur:
+                if ps[a] > 1 and pt[b] > 1:
+                    res = max(res, cur*2 + ps[a] + pt[b])
+                else:
+                    res = max(res, cur*2 + max(ps[a], pt[b]))
+        return res
