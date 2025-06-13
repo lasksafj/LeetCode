bf3704eef69d06ec5cbaedde10469faff488465b
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2025-06-12 21:01:48 -0700

    Time: 225 ms (87.98%), Space: 26.8 MB (82.64%) - LeetHub

--- 3826-maximum-profit-from-valid-topological-order-in-dag/3826-maximum-profit-from-valid-topological-order-in-dag.py
+++ 3826-maximum-profit-from-valid-topological-order-in-dag/3826-maximum-profit-from-valid-topological-order-in-dag.py
@@ -1,38 +1,24 @@
 class Solution:
     def maxProfit(self, n: int, edges: List[List[int]], score: List[int]) -> int:
-        # ind = [0]*n
-        # for a,b in edges:
-        #     ind[b] += 1
-
-        # @cache
-        # def dfs(mask):
-        #     d = n - bin(mask).count('1') + 1
-        #     res = 0
-        #     for i in range(n):
-        #         if (1<<i)&mask and ind[i] & mask == 0:
-        #             res = max(res, dfs(mask^(1<<i)) + score[i]*d)
-        #     return res
-        # mask = (1<<n) - 1
-        # return dfs(mask)
-
-        N = (1<<n)-1
-        ind = defaultdict(int)
+        ind = [0]*n
+        ind_cnt = [0]*n
         for a,b in edges:
-            ind[1<<b] |= 1<<a
-        A = {}
-        for i in range(n):
-            A[1<<i] = score[i]
-        dp = [-inf] * (N+1)
-        dp[N] = 0
-        for mask in range(N, -1, -1):
-            if dp[mask] == -inf:
-                continue
+            ind[b] |= 1<<a
+            ind_cnt[b] += 1
+
+        @cache
+        def dfs(mask, deg):
             d = n - bin(mask).count('1') + 1
-            tmp = mask
-            while tmp:
-                i = tmp&(-tmp)
-                nmask = mask - i
-                if ind[i] & mask == 0:
-                    dp[nmask] = max(dp[nmask], dp[mask] + A[i]*d)
-                tmp -= i
-        return dp[0]
+            res = 0
+            if deg == 0:
+                for a in sorted([i for i in range(n) if mask&(1<<i)], key=lambda x:score[x]):
+                    res += d*score[a]
+                    d += 1
+                return res
+            
+            for i in range(n):
+                if (1<<i)&mask and ind[i] & mask == 0:
+                    res = max(res, dfs(mask^(1<<i), deg-ind_cnt[i]) + score[i]*d)
+            return res
+        mask = (1<<n) - 1
+        return dfs(mask, len(edges))
