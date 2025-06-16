b6c552ee52c8f83db9b772033430e119d8367333
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2025-06-15 18:37:47 -0700

    Time: 319 ms (66.69%), Space: 34.5 MB (51.43%) - LeetHub

--- 3841-concatenated-divisibility/3841-concatenated-divisibility.py
+++ 3841-concatenated-divisibility/3841-concatenated-divisibility.py
@@ -2,19 +2,19 @@ class Solution:
     def concatenatedDivisibility(self, nums: List[int], k: int) -> List[int]:
         nums.sort()
         N = len(nums)
-        track = [-1]*N
+        track = []
         @cache
-        def dfs(i,mask,m):
-            if i == N:
+        def dfs(mask,m):
+            if mask == (1<<N)-1:
                 return m == 0
             for j in range(N):
                 if (1<<j)&mask:
                     continue
                 a = nums[j]
                 b = len(str(a))
-                if dfs(i+1, mask^(1<<j), (m*(10**b) + a) % k):
-                    track[i] = nums[j]
+                if dfs(mask^(1<<j), (m*(10**b) + a) % k):
+                    track.append(nums[j])
                     return True
             return False
-        dfs(0, 0, 0)
-        return track if track[0] >= 0 else []
+        dfs(0, 0)
+        return track[::-1]
