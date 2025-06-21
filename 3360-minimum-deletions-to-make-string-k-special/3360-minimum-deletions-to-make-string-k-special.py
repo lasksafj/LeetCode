a22cfa27b87c3bede873cdd24bb287c07d5c5caf
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2025-06-20 22:42:26 -0700

    Time: 51 ms (93.8%), Space: 17.9 MB (100%) - LeetHub

--- 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.py
+++ 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.py
@@ -7,13 +7,10 @@ class Solution:
         i = j = 0
         res = inf
 
-        for r in range(1, len(word)+1):
-            while i < N and r >= A[i]:
-                rsum -= A[i]
-                i += 1
-            l = r-k
-            while j < N and A[j] < l:
-                lsum += A[j]
+        for i in range(N):
+            while j < N and A[i] + k >= A[j]:
+                rsum -= A[j]
                 j += 1
-            res = min(res, rsum - r*(N-i) + lsum)
+            res = min(res, lsum + rsum - (A[i]+k)*(N-j))
+            lsum += A[i]
         return res
