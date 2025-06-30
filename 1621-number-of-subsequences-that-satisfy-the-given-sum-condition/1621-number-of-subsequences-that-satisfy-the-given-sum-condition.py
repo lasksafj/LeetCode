13d49354678fbe2a2d329510fe38e4d291b24923
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2025-06-29 17:20:36 -0700

    Time: 159 ms (68.13%), Space: 27.8 MB (78.56%) - LeetHub

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/1621-number-of-subsequences-that-satisfy-the-given-sum-condition.py
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/1621-number-of-subsequences-that-satisfy-the-given-sum-condition.py
@@ -6,7 +6,7 @@ class Solution:
         i,j = 0,len(nums)-1
         while i <= j:
             if nums[i] + nums[j] <= target:
-                res = (res + pow(2, j-i)) % MOD
+                res = (res + pow(2, j-i, MOD)) % MOD
                 i += 1
             else:
                 j -= 1
