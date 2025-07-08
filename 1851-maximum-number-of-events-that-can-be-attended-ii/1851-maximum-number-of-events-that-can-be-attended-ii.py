ac14c5e305544f78cc107f63c910bdfc8b49ff0b
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2025-07-07 20:55:24 -0700

    Time: 503 ms (92.31%), Space: 57.9 MB (86.26%) - LeetHub

--- 1851-maximum-number-of-events-that-can-be-attended-ii/1851-maximum-number-of-events-that-can-be-attended-ii.py
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/1851-maximum-number-of-events-that-can-be-attended-ii.py
@@ -3,12 +3,17 @@ class Solution:
         events.sort(key=lambda x:x[1])
         events = [[0,0,0]] + events
         N = len(events)
+        prev = [-1]*N
+        for i in range(1,N):
+            a,b,c = events[i]
+            j = bisect_right(events, a-1, key=lambda x:x[1])-1
+            prev[i] = j
         dp = [0]*N
         for t in range(k):
             ndp = [0]*N
             for i in range(1, N):
                 a,b,v = events[i]
-                j = bisect_right(events, a-1, key=lambda x:x[1])-1
+                j = prev[i]
                 ndp[i] = max(ndp[i-1], dp[j] + v)
             dp = ndp
         return dp[-1]
