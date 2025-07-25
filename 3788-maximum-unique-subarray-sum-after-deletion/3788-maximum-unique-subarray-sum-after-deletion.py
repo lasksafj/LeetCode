bac8b1062ff1294a9d98401f2d0740c8d3c9efff
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2025-07-24 17:27:45 -0700

    Time: 0 ms (100%), Space: 17.7 MB (85.25%) - LeetHub

--- 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.py
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.py
@@ -1,4 +1,4 @@
 class Solution:
     def maxSum(self, nums: List[int]) -> int:
         A = set(n for n in nums if n >= 0)
-        return sum(A) if len(A) > 0 else max(n for n in nums if n < 0)
+        return sum(A) if len(A) > 0 else max(nums)
