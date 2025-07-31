bb66d1be28795c9fbacf975b7e824fd840e86e3f
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2025-07-30 17:32:42 -0700

    Time: 455 ms (43.45%), Space: 44.8 MB (51.19%) - LeetHub

--- 0934-bitwise-ors-of-subarrays/0934-bitwise-ors-of-subarrays.py
+++ 0934-bitwise-ors-of-subarrays/0934-bitwise-ors-of-subarrays.py
@@ -1,13 +1,9 @@
 class Solution:
     def subarrayBitwiseORs(self, arr: List[int]) -> int:
         N = len(arr)
-        mp = defaultdict(lambda:-1)
-        for i in range(N):
-            a = 0
-            for j in range(i, -1, -1):
-                a |= arr[j]
-                if mp[a] >= j-1 and mp[a] < i:
-                    mp[a] = i
-                    break
-                mp[a] = i
-        return len(mp)
+        res = set()
+        s = {0} # all unique OR of subarr ending at j, len of this <= 32, b/c OR arr[i,j] >= arr[i+1,j]
+        for n in arr:
+            s = {a | n for a in s} | {n}
+            res |= s
+        return len(res)
