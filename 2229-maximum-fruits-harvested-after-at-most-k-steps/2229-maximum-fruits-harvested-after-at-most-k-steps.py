b2328bc41b4e2ed6c015e63d681715bf71590503
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2025-08-03 00:15:36 -0700

    Time: 95 ms (87.5%), Space: 52.4 MB (50.83%) - LeetHub

--- 2229-maximum-fruits-harvested-after-at-most-k-steps/2229-maximum-fruits-harvested-after-at-most-k-steps.py
+++ 2229-maximum-fruits-harvested-after-at-most-k-steps/2229-maximum-fruits-harvested-after-at-most-k-steps.py
@@ -1,22 +1,21 @@
 class Solution:
     def maxTotalFruits(self, fruits: List[List[int]], startPos: int, k: int) -> int:
-        pre = [fruits[0][1]]
-        for _,f in fruits[1:]:
-            pre.append(pre[-1] + f)
-        pre.append(pre[-1])
-        res = 0
-        for i,(p,f) in enumerate(fruits):
-            l = max(0, startPos - p)
-            r = k - 2*l
-            if r < 0: continue
-            j = bisect_right(fruits, startPos+r, key=lambda x:x[0])-1
-            if j > -1:
-                res = max(res, pre[j] - (pre[i-1] if i else 0))
-        for j in range(len(fruits)-1,-1,-1):
-            p = fruits[j][0]
-            r = max(0, p - startPos)
-            l = k - 2*r
-            if l < 0: continue
-            i = bisect_left(fruits, startPos-l, key=lambda x:x[0])
-            res = max(res, pre[j] - (pre[i-1] if i else 0))
+        N = len(fruits)
+        res = score = 0
+        j = 0
+        for i in range(N):
+            score += fruits[i][1]
+            while j <= i:
+                a = startPos - fruits[j][0]
+                b = fruits[i][0] - startPos
+                if a <= 0:
+                    d = b
+                elif b <= 0:
+                    d = a
+                else:
+                    d = min(a,b)*2 + max(a,b)
+                if d <= k: break
+                score -= fruits[j][1]
+                j += 1
+            res = max(res, score)
         return res
