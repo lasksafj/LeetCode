72cdb6e768f54b8447a3f09ce51f66c755f1b093
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2025-08-07 10:09:47 -0700

    Time: 1727 ms (22.81%), Space: 189.4 MB (15.79%) - LeetHub

--- 3648-find-the-maximum-number-of-fruits-collected/3648-find-the-maximum-number-of-fruits-collected.py
+++ 3648-find-the-maximum-number-of-fruits-collected/3648-find-the-maximum-number-of-fruits-collected.py
@@ -7,23 +7,23 @@ class Solution:
             fruits[i][i] = 0
         @cache
         def dfs1(i, j):
-            if i == N-1:
-                return 0 if j == N-1 else -inf
-            res = -1
+            if j == N or j <= i:
+                return -inf
+            if i == N:
+                return 0
+            res = 0
             for nj in [j-1,j,j+1]:
-                if (N+1)//2 <= nj < N:
-                    res = max(res, dfs1(i+1, nj))
-            if res == -1: return -inf
+                res = max(res, dfs1(i+1, nj))
             return res + fruits[i][j]
         @cache
         def dfs2(i,j):
-            if j == N-1:
-                return 0 if i == N-1 else -inf
-            res = -1
+            if i == N or i <= j:
+                return -inf
+            if j == N:
+                return 0
+            res = 0
             for ni in [i-1,i,i+1]:
-                if (N+1)//2 <= ni < N:
-                    res = max(res, dfs2(ni, j+1))
-            if res == -1: return -inf
+                res = max(res, dfs2(ni, j+1))
             return res + fruits[i][j]
         
         return res + dfs1(0,N-1) + dfs2(N-1,0)
