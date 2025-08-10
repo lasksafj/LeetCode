47cd8e19063af73b8ddfee91a9137fbd0728d916
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2025-08-09 18:42:48 -0700

    Time: 3 ms (59.16%), Space: 17.6 MB (79.77%) - LeetHub

--- 0900-reordered-power-of-2/0900-reordered-power-of-2.py
+++ 0900-reordered-power-of-2/0900-reordered-power-of-2.py
@@ -1,10 +1,7 @@
 class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:
-        if n == 1: return True
-        n = str(n)
-        for d in permutations(n, len(n)):
-            if d[0] == '0' or d[-1] in ['1','3','5','7','9']: continue
-            d = int(''.join(d))
-            if d&(d-1) == 0:
+        n = sorted(str(n))
+        for i in range(32):
+            if sorted(str(1<<i)) == n:
                 return True
         return False
