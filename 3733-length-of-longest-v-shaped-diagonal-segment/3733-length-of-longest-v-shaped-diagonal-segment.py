04bce788cf83a04a27eca7f0f09372a0cf573832
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2025-08-27 12:04:32 -0700

    Time: 9837 ms (5.11%), Space: 269.2 MB (72.26%) - LeetHub

--- 3733-length-of-longest-v-shaped-diagonal-segment/3733-length-of-longest-v-shaped-diagonal-segment.py
+++ 3733-length-of-longest-v-shaped-diagonal-segment/3733-length-of-longest-v-shaped-diagonal-segment.py
@@ -2,26 +2,21 @@ class Solution:
     def lenOfVDiagonal(self, grid: List[List[int]]) -> int:
         M,N = len(grid),len(grid[0])
         @cache
-        def dfs(i,j,di,dj,turn):
-            way = [(di,dj,turn)]
+        def dfs(x,y,dx,dy,turn):
+            D = [[dx,dy,turn]]
             if turn:
-                way.append((dj,-di,0))
+                D += [[dy,-dx,0]]
             res = 0
-            for dx,dy,t in way:
-                ni,nj = i+dx,j+dy
-                if 0<=ni<M and 0<=nj<N and grid[ni][nj] != grid[i][j] and grid[ni][nj] != 1:
-                    if grid[i][j] != 1 or (grid[i][j] == 1 and grid[ni][nj] == 2):
-                        res = max(res, dfs(ni,nj,dx,dy,t))
-            return res + 1
+            for ndx,ndy,t in D:
+                nx,ny = x+ndx,y+ndy
+                if 0 <= nx < M and 0 <= ny < N and grid[nx][ny] != grid[x][y] and grid[nx][ny] != 1:
+                    if grid[x][y] != 1 or (grid[x][y] == 1 and grid[nx][ny] == 2):
+                        res = max(res, dfs(nx,ny,ndx,ndy,t))
+            return res+1
         res = 0
         for i in range(M):
             for j in range(N):
                 if grid[i][j] == 1:
-                    res = max(res, 
-                        dfs(i,j,1,1,1),
-                        dfs(i,j,1,-1,1),
-                        dfs(i,j,-1,-1,1),
-                        dfs(i,j,-1,1,1)
-                    )
-
+                    for dx,dy in [[1,1],[-1,1],[-1,-1],[1,-1]]:
+                        res = max(res, dfs(i,j,dx,dy,1))
         return res
