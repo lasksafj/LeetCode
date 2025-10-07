6a84b2eeb34b6e4f99f9e7406628d02135328bb8
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2025-10-06 22:14:58 -0700

    Time: 83 ms (96.74%), Space: 50.4 MB (19.07%) - LeetHub

--- 1612-avoid-flood-in-the-city/1612-avoid-flood-in-the-city.py
+++ 1612-avoid-flood-in-the-city/1612-avoid-flood-in-the-city.py
@@ -2,19 +2,34 @@ class Solution:
     def avoidFlood(self, rains: List[int]) -> List[int]:
         N = len(rains)
         res = [-1]*N
-        A = SortedList()
+        next_zero = [-1]*N
+        k = N
+        zeros = set()
+        for i in range(N-1,-1,-1):
+            next_zero[i] = k
+            if rains[i] == 0:
+                k = i
+                zeros.add(i)
+        def find(i):
+            res = next_zero[i]
+            if res == N:
+                return N
+            if next_zero[i] not in zeros:
+                res = find(next_zero[i])
+            next_zero[i] = res
+            return res
+        
         last_idx = {}
         for i,r in enumerate(rains):
             if r > 0:
                 if r in last_idx:
-                    j = A.bisect_left(last_idx[r])
-                    if j == len(A):
+                    j = find(last_idx[r])
+                    if j > i:
                         return []
-                    res[A[j]] = r
-                    A.pop(j)
+                    res[j] = r
+                    zeros.remove(j)
                 last_idx[r] = i
-            else:
-                A.add(i)
+
         for i in range(N):
             if rains[i] == 0 and res[i] == -1:
                 res[i] = 1
