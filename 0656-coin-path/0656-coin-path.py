877002424586f1ef3a57258da77dcd4803f0bb32
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2025-10-20 00:38:08 -0700

    Time: 7 ms (90.55%), Space: 18.1 MB (69.89%) - LeetHub

--- 0656-coin-path/0656-coin-path.py
+++ 0656-coin-path/0656-coin-path.py
@@ -2,31 +2,23 @@ class Solution:
     def cheapestJump(self, coins: List[int], maxJump: int) -> List[int]:
         N = len(coins)
         dp = [inf]*N
-        prev = [-1]*N
-        dp[0] = coins[0]
+        dp[-1] = coins[-1]
         st = deque()
-        L = [1]*N
-        for i in range(N):
+        for i in range(N-1,-1,-1):
             if coins[i] == -1:
                 continue
-            while st and st[0] < i-maxJump:
+            while st and st[0] > i+maxJump:
                 st.popleft()
             if st:
                 dp[i] = dp[st[0]] + coins[i]
-                ma = st[0]
-                for j in st:
-                    if dp[j] > dp[st[0]]:
-                        break
-                    ma = max([ma, j], key=lambda e:[L[e], -e])
-                prev[i] = ma
-                L[i] = L[ma] + 1
             while st and dp[st[-1]] > dp[i]:
                 st.pop()
             st.append(i)
-        if dp[-1] == inf: return []
-        i = N-1
-        A = []
-        while i != -1:
-            A.append(i+1)
-            i = prev[i]
-        return A[::-1]
+        if dp[0] == inf: return []
+        res = []
+        mi = dp[0]
+        for i in range(N):
+            if dp[i] == mi:
+                res.append(i+1)
+                mi -= coins[i]
+        return res
