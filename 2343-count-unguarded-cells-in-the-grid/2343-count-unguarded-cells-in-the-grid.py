6a4ba3563e12cf04fe66dae1d0fd11854ac4498e
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2025-11-02 11:26:42 -0800

    Time: 188 ms (99.49%), Space: 39.1 MB (68.37%) - LeetHub

--- 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.py
+++ 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.py
@@ -1,27 +1,25 @@
 class Solution:
     def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
         A = [[0]*n for _ in range(m)]
-        def f(guards, walls, m,n, t):
-            mat = [[0]*n for _ in range(m)]
-            for a,b in guards:
-                mat[a][b] = 1
-            for a,b in walls:
-                mat[a][b] = -1
-            for i in range(m):
-                g = 0
-                for j in range(n):
-                    if mat[i][j] != 0:
-                        g = mat[i][j]
-                    a,b = (i,j) if t else (j,i)
-                    A[a][b] = max(A[a][b], max(g, 0))
-                g = 0
-                for j in range(n-1,-1,-1):
-                    if mat[i][j] != 0:
-                        g = mat[i][j]
-                    a,b = (i,j) if t else (j,i)
-                    A[a][b] = max(A[a][b], max(g, 0))
-        f(guards, walls, m, n, 1)
-        guards = [[b,a] for a,b in guards]
-        walls = [[b,a] for a,b in walls]
-        f(guards, walls, n, m, 0)
-        return m*n - sum(sum(r) for r in A) - len(walls)
+        for a,b in guards:
+            A[a][b] = 1
+        for a,b in walls:
+            A[a][b] = 1
+        for i,j in guards:
+            ni = i+1
+            while ni < m and A[ni][j] != 1:
+                A[ni][j] = -1
+                ni += 1
+            ni = i-1
+            while ni >= 0 and A[ni][j] != 1:
+                A[ni][j] = -1
+                ni -= 1
+            nj = j+1
+            while nj < n and A[i][nj] != 1:
+                A[i][nj] = -1
+                nj += 1
+            nj = j-1
+            while nj >= 0 and A[i][nj] != 1:
+                A[i][nj] = -1
+                nj -= 1
+        return sum(1 if e==0 else 0 for row in A for e in row)
