d456e68c9d63bbc02ac368cb6fd9bb18d48178f6
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2025-11-05 21:50:49 -0800

    Time: 931 ms (14.95%), Space: 107.8 MB (56.13%) - LeetHub

--- 3863-power-grid-maintenance/3863-power-grid-maintenance.py
+++ 3863-power-grid-maintenance/3863-power-grid-maintenance.py
@@ -1,36 +1,36 @@
 class Solution:
     def processQueries(self, c: int, connections: List[List[int]], queries: List[List[int]]) -> List[int]:
-        adj = defaultdict(list)
+        f = list(range(c+1))
+        r = [0]*(c+1)
+        def root(x):
+            while x != f[x]:
+                x = f[x]
+            return x
+        def union(a,b):
+            a,b = root(a),root(b)
+            if a == b:
+                return
+            if r[a] < r[b]:
+                a,b = b,a
+            f[b] = a
+            if r[a] == r[b]:
+                r[a] += 1
+
         for a,b in connections:
-            adj[a].append(b)
-            adj[b].append(a)
-        vis = set()
-        cur = set()
-        def dfs(i):
-            cur.add(i)
-            for ne in adj[i]:
-                if ne not in cur:
-                    dfs(ne)
-        mp = {}
-        p = [0]*(c+1)
+            union(a,b)
+        mp = defaultdict(lambda:SortedList())
         for i in range(1,c+1):
-            if i not in vis:
-                cur = set()
-                dfs(i)
-                vis |= cur
-                cur = SortedList(list(cur))
-                for j in cur:
-                    p[j] = cur[0]
-                mp[cur[0]] = cur
+            mp[root(i)].add(i)
         res = []
         off = set()
         for t,x in queries:
             if t == 1:
                 if x in off:
-                    res.append(mp[p[x]][0] if mp[p[x]] else -1)
+                    rx = root(x)
+                    res.append(mp[rx][0] if mp[rx] else -1)
                 else:
                     res.append(x)
             else:
-                mp[p[x]].discard(x)
+                mp[root(x)].discard(x)
                 off.add(x)
         return res
