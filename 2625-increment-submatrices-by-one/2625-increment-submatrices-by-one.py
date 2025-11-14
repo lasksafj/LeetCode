983e162d3b48f1d34fedbd4b3678095a9d32e11c
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2025-11-13 16:50:06 -0800

    Time: 100 ms (99.06%), Space: 39.9 MB (45.07%) - LeetHub

--- 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.py
+++ 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.py
@@ -1,11 +1,14 @@
 class Solution:
     def rangeAddQueries(self, n: int, queries: List[List[int]]) -> List[List[int]]:
+        A = [[0]*(n+1) for _ in range(n+1)]
+        for r1,c1,r2,c2 in queries:
+            A[r1][c1] += 1
+            A[r1][c2+1] -= 1
+            A[r2+1][c1] -= 1
+            A[r2+1][c2+1] += 1
+        cur_diff = [0]*(n+1)
         res = []
-        for i in range(n):
-            A = [0]*(n+1)
-            for a,b,c,d in queries:
-                if a <= i <= c:
-                    A[b] += 1
-                    A[d+1] -= 1
-            res.append(list(accumulate(A))[:-1])
+        for row in A[:-1]:
+            cur_diff = [c+r for c,r in zip(cur_diff,row)]
+            res.append(list(accumulate(cur_diff))[:-1])
         return res
