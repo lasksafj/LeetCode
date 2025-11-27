ddd246d892bfd6d6613cff5548315102f1aaef25
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2025-11-26 19:32:38 -0800

    Time: 0 ms (100%), Space: 18 MB (42.42%) - LeetHub

---
+++ 0364-nested-list-weight-sum-ii/0364-nested-list-weight-sum-ii.py
@@ -0,0 +1,61 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def depthSumInverse(self, nestedList: List[NestedInteger]) -> int:
+        def dfs(cur):
+            d = 0
+            for ne in cur.getList():
+                d = max(d, dfs(ne))
+            return d+1
+        max_depth = max([dfs(cur) for cur in nestedList])
+        res = 0
+        def dfs1(cur, d):
+            nonlocal res
+            for ne in cur.getList():
+                dfs1(ne, d+1)
+            if cur.getInteger():
+                res += cur.getInteger() * (max_depth - d + 1)
+        for cur in nestedList:
+            dfs1(cur, 1)
+        return res
