ec30684d0e30752547f0f6ee96fb5f4cbc7dd393
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2025-12-01 23:59:00 -0800

    Time: 76 ms (77.33%), Space: 63.4 MB (91.07%) - LeetHub

--- 3623-count-number-of-trapezoids-i/3623-count-number-of-trapezoids-i.py
+++ 3623-count-number-of-trapezoids-i/3623-count-number-of-trapezoids-i.py
@@ -3,12 +3,9 @@ class Solution:
         mp = defaultdict(int)
         for x,y in points:
             mp[y] += 1
-        A = []
+        t = res = 0
         for a in mp.values():
-            A.append(comb(a,2))
-        t = 0
-        res = 0
-        for a in A:
-            res += a*t
-            t += a
+            n_edges = a*(a-1)//2
+            res += n_edges * t
+            t += n_edges
         return res % (10**9+7)
