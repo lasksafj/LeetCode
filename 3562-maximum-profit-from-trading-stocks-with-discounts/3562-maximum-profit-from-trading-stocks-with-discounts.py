f71ec41a800bb2a80031a0490e8126f86626918d
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2025-12-10 11:45:34 -0800

    Time: 14720 ms (23.22%), Space: 18.6 MB (76.79%) - LeetHub

---
+++ 3562-maximum-profit-from-trading-stocks-with-discounts/3562-maximum-profit-from-trading-stocks-with-discounts.py
@@ -0,0 +1,30 @@
+def merge(A, B):
+    N = len(A)
+    res = [-inf]*N
+    for i in range(N):
+        for j in range(N-i):
+            res[i+j] = max(res[i+j], A[i] + B[j])
+    return res
+
+class Solution:
+    def maxProfit(self, n: int, present: List[int], future: List[int], hierarchy: List[List[int]], budget: int) -> int:
+        adj = defaultdict(list)
+        for a,b in hierarchy:
+            adj[a-1].append(b-1)
+        def dfs(i):
+            dp0 = [0]*(budget+1)
+            dp1 = [0]*(budget+1)
+            for ne in adj[i]:
+                D, no_D = dfs(ne)
+                dp1 = merge(dp1, D)
+                dp0 = merge(dp0, no_D)
+            res0 = dp0[:]
+            res1 = dp0[:]
+            cost = present[i]
+            for b in range(cost, len(dp1)):
+                res0[b] = max(dp1[b-cost] + future[i]-cost, res0[b])
+            cost //= 2
+            for b in range(cost, len(dp1)):
+                res1[b] = max(dp1[b-cost] + future[i]-cost, res1[b])
+            return res1, res0
+        return dfs(0)[1][-1]
