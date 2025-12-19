ff7cc4b115065d739d0afe7be72b20835b17e1bf
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2025-12-18 17:59:18 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
@@ -1,37 +1,17 @@
-def f(edges, q):
-    adj = defaultdict(list)
-    for a,b in edges:
-        adj[a].append(b)
-        adj[b].append(a)
-    q = deque(set(q))
-    res = set(q)
-    while q:
-        a = q.popleft()
-        for ne in adj[a]:
-            if ne not in res:
-                res.add(ne)
-                q.append(ne)
-    return res
-
-
 class Solution:
     def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
-        meetings.sort(key=lambda x: x[2])
-        i = 0
-        N = len(meetings)
-        res = {0, firstPerson}
-        while i < N:
-            j = i
-            start = []
-            edges = []
-            while j < N and meetings[j][2] == meetings[i][2]:
-                a,b,_ = meetings[j]
-                if a in res:
-                    start.append(a)
-                if b in res:
-                    start.append(b)
-                edges.append([a,b])
-                j += 1
-            res |= f(edges, start)
-            i = j
-        return list(res)
+        adj = defaultdict(list)
+        adj[0].append([firstPerson, 0])
+        secret_time = [inf]*n
+        for a,b,t in meetings:
+            adj[a].append([b,t])
+            adj[b].append([a,t])
+
+        res = set()
+        def dfs(i,t):
+            secret_time[i] = t
+            for ne,nt in adj[i]:
+                if secret_time[ne] > nt and t <= nt:
+                    dfs(ne, nt)
+        dfs(0, 0)
+        return [i for i,t in enumerate(secret_time) if t < inf]
