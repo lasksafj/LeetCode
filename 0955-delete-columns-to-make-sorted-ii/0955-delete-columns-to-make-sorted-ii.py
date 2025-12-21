92083d7d6213964e857f1c828db2defe2e45f82e
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2025-12-21 00:49:50 -0800

    Time: 3 ms (79.49%), Space: 17.3 MB (100%) - LeetHub

--- 0955-delete-columns-to-make-sorted-ii/0955-delete-columns-to-make-sorted-ii.py
+++ 0955-delete-columns-to-make-sorted-ii/0955-delete-columns-to-make-sorted-ii.py
@@ -1,17 +1,17 @@
 class Solution:
     def minDeletionSize(self, strs: List[str]) -> int:
+        N = len(strs)
         res = 0
-        is_sorted = [0]*len(strs)
+        A = [1]*(N-1)
         for j in range(len(strs[0])):
             ok = True
-            is_sorted2 = is_sorted[:]
-            for i in range(len(strs)-1):
-                if strs[i][j] > strs[i+1][j] and is_sorted[i] == 0:
+            for i in range(N-1):
+                if A[i] and strs[i][j] > strs[i+1][j]:
                     res += 1
                     ok = False
                     break
-                if strs[i][j] < strs[i+1][j]:
-                    is_sorted2[i] = 1
             if ok:
-                is_sorted = is_sorted2[:]
+                for i in range(N-1):
+                    if A[i] and strs[i][j] < strs[i+1][j]:
+                        A[i] = 0
         return res
