c4560217b6eed5d25c214a50912c344581534ce0
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2025-12-21 23:20:43 -0800

    Time: 4 ms (57.98%), Space: 18.4 MB (99.35%) - LeetHub

--- 0549-binary-tree-longest-consecutive-sequence-ii/0549-binary-tree-longest-consecutive-sequence-ii.py
+++ 0549-binary-tree-longest-consecutive-sequence-ii/0549-binary-tree-longest-consecutive-sequence-ii.py
@@ -11,36 +11,25 @@ class Solution:
             nonlocal res
             if not r:
                 return 0,0
-            inc_l, dec_l = dfs(r.left)
-            inc_r, dec_r = dfs(r.right)
-            if r.left and r.right:
+            b = r.val
+            inc_l, dec_l = 1, 1
+            L = dfs(r.left)
+            if r.left:
                 a = r.left.val
-                b = r.val
-                c = r.right.val
-                if a+1 == b and b+1 == c:
-                    res = max(res, inc_l + dec_r + 1)
-                elif c+1 == b and b+1 == a:
-                    res = max(res, inc_r + dec_l + 1)
-            
-            if r.left and r.left.val + 1 == r.val:
-                inc_l += 1
-            else:
-                inc_l = 1
-            if r.right and r.right.val + 1 == r.val:
-                inc_r += 1
-            else:
-                inc_r = 1
-            if r.left and r.left.val - 1 == r.val:
-                dec_l += 1
-            else:
-                dec_l = 1
-            if r.right and r.right.val - 1 == r.val:
-                dec_r += 1
-            else:
-                dec_r = 1
-            res = max(res, inc_l, dec_l, inc_r, dec_r)
-
+                if a+1 == b:
+                    inc_l = L[0] + 1
+                if a == b+1:
+                    dec_l = L[1] + 1
+            inc_r, dec_r = 1, 1
+            R = dfs(r.right)
+            if r.right:
+                a = r.right.val
+                if a+1 == b:
+                    inc_r = R[0] + 1
+                if a == b+1:
+                    dec_r = R[1] + 1
+            res = max(res, inc_l + dec_r - 1, inc_r + dec_l - 1)
             return max(inc_l, inc_r), max(dec_l, dec_r)
-        
+
         dfs(root)
         return res
