88e188b1c348be3594b0ffc72f901d0a856e9fe1
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2025-12-21 22:52:51 -0800

    Time: 155 ms (16%), Space: 17.3 MB (100%) - LeetHub

--- 0960-delete-columns-to-make-sorted-iii/0960-delete-columns-to-make-sorted-iii.py
+++ 0960-delete-columns-to-make-sorted-iii/0960-delete-columns-to-make-sorted-iii.py
@@ -1,13 +1,9 @@
 class Solution:
     def minDeletionSize(self, strs: List[str]) -> int:
-        def lte(a,b):
-            return (a == -1) or all(strs[i][a] <= strs[i][b] for i in range(len(strs)))
-        @cache
-        def dfs(i, j):
-            if i == len(strs[0]):
-                return 0
-            res = dfs(i+1, j) + 1
-            if not lte(j, i):
-                return res
-            return min(dfs(i+1, i), res)
-        return dfs(0, -1)
+        M,N = len(strs),len(strs[0])
+        dp = [1]*N
+        for i in range(N):
+            for j in range(i):
+                if all(strs[k][j] <= strs[k][i] for k in range(M)):
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return N - max(dp)
