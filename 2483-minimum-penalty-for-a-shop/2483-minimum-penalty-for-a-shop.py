9bef56cca53e787c1a633aa49b8f58b4e02747cb
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2025-12-25 19:14:49 -0800

    Time: 402 ms (5.01%), Space: 18.2 MB (55.15%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.py
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.py
@@ -1,16 +1,10 @@
 class Solution:
     def bestClosingTime(self, customers: str) -> int:
-        N = len(customers)
-        pre = [0]*N
-        pre[0] = (customers[0] == 'N')
-        for i in range(1,N):
-            pre[i] = pre[i-1] + (customers[i] == 'N')
-        p = 0
-        pen = pre[-1]
-        res = N
-        for i in range(N-1, -1, -1):
-            p += (customers[i] == 'Y')
-            if (pre[i-1] if i>0 else 0) + p <= pen:
-                pen = pre[i-1] + p
-                res = i
-        return res
+        y = customers.count('Y')
+        n = 0
+        res = [0,y+n]
+        for i,c in enumerate(customers):
+            res = min(res, [i, y+n], key=lambda x: x[1])
+            y -= c == 'Y'
+            n += c == 'N'
+        return min(res, [i+1, y+n], key=lambda x: x[1])[0]
