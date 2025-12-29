324549ac9fe8fdb938329125f1d9796b7481fe80
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2025-12-28 20:46:16 -0800

    Time: 450 ms (52.97%), Space: 199.1 MB (6.93%) - LeetHub

--- 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.py
+++ 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.py
@@ -4,15 +4,17 @@ class Solution:
         for a,b,c in allowed:
             mp[a,b].append(c)
         N = len(bottom)
-        def dfs(i, j, A):
-            if i == N:
+        @cache
+        def dfs(last, cur):
+            if len(last) == 1:
                 return True
-            if i+j == N:
-                return dfs(i+1, 0, A + [''])
-            for c in mp[A[i-1][j], A[i-1][j+1]]:
-                A[-1] += c
-                if dfs(i, j+1, A):
+            if len(cur) == len(last)-1:
+                return dfs(cur, '')
+            j = len(cur)
+            for c in mp[last[j], last[j+1]]:
+                cur += c
+                if dfs(last, cur):
                     return True
-                A[-1] = A[-1][:-1]
+                cur = cur[:-1]
             return False
-        return dfs(1, 0, [bottom, ''])
+        return dfs(bottom, '')
