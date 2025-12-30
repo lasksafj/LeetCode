fff3f35a8716ed2e843ceb8bffad3b7592dc78ac
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2025-12-29 22:24:33 -0800

    Time: 4 ms (12.12%), Space: 17.2 MB (97.58%) - LeetHub

--- 0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
@@ -1,28 +1,21 @@
 class Solution:
     def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
+        def check(A):
+            B = A[0] + A[1] + A[2]
+            if len(set(B)) < 9 or max(B) > 9 or min(B) < 1:
+                return 0
+            if sum(A[0]) != sum(A[1]) or sum(A[1]) != sum(A[2]):
+                return 0
+            if A[0][0] + A[2][2] != A[0][2] + A[2][0]:
+                return 0
+            A = list(zip(*A))
+            if sum(A[0]) != sum(A[1]) or sum(A[1]) != sum(A[2]):
+                return 0
+            return 1
+            
         M,N = len(grid),len(grid[0])
-        def check(si,sj):
-            if M-si<3 or N-sj<3:
-                return False
-            sr,sc = [0]*3,[0]*3
-            sd1,sd2 = 0,0
-            vis = set()
-            for i in range(si,si+3):
-                for j in range(sj,sj+3):
-                    n = grid[i][j]
-                    if n<1 or n>9 or n in vis:
-                        return False
-                    vis.add(n)
-                    r,c = i-si,j-sj
-                    sr[r] += n
-                    sc[c] += n
-                    if r==c:
-                        sd1 += n
-                    if r==2-c:
-                        sd2 += n
-            return sr[0]==sr[1] and sr[1]==sr[2] and sc[0]==sc[1] and sc[1]==sc[2] and sr[0]==sd1 and sd1==sd2
         res = 0
-        for i in range(M):
-            for j in range(N):
-                res += check(i,j)
+        for i in range(M-2):
+            for j in range(N-2):
+                res += check([grid[i][j:j+3]] + [grid[i+1][j:j+3]] + [grid[i+2][j:j+3]])
         return res
