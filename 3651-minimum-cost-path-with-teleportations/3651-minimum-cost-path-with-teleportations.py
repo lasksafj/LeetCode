b20be74dc1e6c7f46e0281776bfc5d0336473165
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2026-01-28 01:06:22 -0800

    Time: 13372 ms (5%), Space: 24.6 MB (48%) - LeetHub

---
+++ 3651-minimum-cost-path-with-teleportations/3651-minimum-cost-path-with-teleportations.py
@@ -0,0 +1,44 @@
+class BIT:
+    def __init__(self, N):
+        self.N = N
+        self.T = [inf]*(N+1)
+
+    def update(self, index: int, val: int) -> None:
+        i = index+1
+        while i <= self.N:
+            self.T[i] = min(self.T[i], val)
+            i += i&(-i)
+    
+    def get_min(self, index):
+        res = inf
+        i = index+1
+        while i > 0:
+            res = min(res, self.T[i])
+            i -= i&(-i)
+        return res
+
+class Solution:
+    def minCost(self, grid: List[List[int]], k: int) -> int:
+        M,N = len(grid),len(grid[0])
+        A = [[i,j,grid[i][j]] for i in range(M) for j in range(N)]
+        A.sort(key=lambda e:-e[2])
+        mp = {}
+        mp[A[-1][0],A[-1][1]] = len(A)-1
+        for i in range(len(A)-2, -1, -1):
+            if A[i][2] == A[i+1][2]:
+                mp[A[i][0], A[i][1]] = mp[A[i+1][0], A[i+1][1]]
+            else:
+                mp[A[i][0], A[i][1]] = i
+        bit = BIT(len(A))
+        dp = [[[inf] * (k+1) for _ in range(N)] for _ in range(M)]
+        for d in range(k+1):
+            for i in range(M):
+                for j in range(N):
+                    if (i,j) == (0,0):
+                        dp[i][j][d] = 0
+                    else:
+                        dp[i][j][d] = min(min(dp[i-1][j][d], dp[i][j-1][d]) + grid[i][j], bit.get_min(mp[i,j]))
+            for i in range(M):
+                for j in range(N):
+                    bit.update(mp[i,j], dp[i][j][d])
+        return dp[M-1][N-1][k]
