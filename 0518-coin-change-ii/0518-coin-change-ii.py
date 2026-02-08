e01f57e15b8c506965567da615d22c9d23e05924
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2026-02-08 11:39:21 -0800

    Time: 211 ms (74.85%), Space: 19.6 MB (59.87%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,20 +1,7 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        # dp = [0]*(amount+1)
-        # dp[0] = 1
-        # for a in range(1, amount+1):
-        #     for c in coins:
-        #         if a-c >= 0:
-        #             dp[a] += dp[a-c] + 1
-        # return dp[amount]
-        
-        @cache
-        def dfs(i,a):
-            if a == 0:
-                return 1
-            if i == len(coins):
-                return 0
-            if a < coins[i]:
-                return dfs(i+1,a)
-            return dfs(i+1,a) + dfs(i, a-coins[i])
-        return dfs(0,amount)
+        dp = [1] + [0] * amount
+        for c in coins:
+            for a in range(c, amount+1):
+                dp[a] += dp[a-c]
+        return dp[amount]
