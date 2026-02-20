3bc1c54ad5a390512653a2ece726609f2c6f441f
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2026-02-19 21:12:14 -0800

    Time: 1 ms (27.37%), Space: 19.2 MB (65.79%) - LeetHub

--- 0761-special-binary-string/0761-special-binary-string.py
+++ 0761-special-binary-string/0761-special-binary-string.py
@@ -1,5 +1,11 @@
 class Solution:
     def makeLargestSpecial(self, s: str) -> str:
+        # define: mountain: 11.1100..00, no 1 = no 0
+        # seperate s as many mountain as possible
+        # define sol(s): answer function
+        # s -> m1m2...mk
+        # answer = sorted(sol(m1), sol(m2), ...)[::-1]
+        # use cnt to determine if substring is a mountain, if 1: cnt++ else cnt--, but this can make mistake on substring like 11011000, so we cut first 1 and last 0, we run sol('1' + sub[1:-1] + '0')
         def sol(s):
             if not s: return ''
             cnt = 0
