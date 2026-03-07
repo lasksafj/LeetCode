947c6c7cf5f975561a72c2e2085faeab7a4f8e7c
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2026-03-06 22:57:45 -0800

    Time: 467 ms (45.7%), Space: 20 MB (84.68%) - LeetHub

--- 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.py
+++ 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.py
@@ -1,21 +1,22 @@
 class Solution:
     def minFlips(self, s: str) -> int:
-        def f(s):
-            n1,n0 = 0,0
-            res = []
-            for c in s:
-                n0,n1 = n1 + (c!='0'), n0 + (c!='1')
-                res.append([n0,n1])
-            return res
-        A = f(s)
-        res = min(A[-1])
-        if len(s)&1 == 0:
-            return res
-        B = f(s[::-1])[::-1]
-        for i in range(len(s)-1):
-            if s[i]==s[i+1]:
-                if s[i] == '0':
-                    res = min(res, A[i][0] + B[i+1][0], 2 + A[i][1] + B[i+1][1])
-                else:
-                    res = min(res, A[i][1] + B[i+1][1], 2 + A[i][0] + B[i+1][0])
+        # mask:
+        # op[0]: 0101...
+        # op[1]: 1010...
+        n = len(s)
+        op = [0,0]
+        for i in range(n):
+            c = ord(s[i])
+            op[0] += (c^i) & 1
+            op[1] += (c^(~i)) & 1
+        res = min(op)
+        for i in range(n):
+            c = ord(s[i])
+            op[0] -= (c^i) & 1
+            op[0] += (c^(i+n)) & 1
+
+            op[1] -= (c^(~i)) & 1
+            op[1] += (c^(~(i+n))) & 1
+
+            res = min(res, min(op))
         return res
