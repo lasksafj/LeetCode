db545e8e11e5d48711af9aae0327ffca6671fb32
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2026-03-08 00:45:55 -0800

    Time: 0 ms (100%), Space: 19.2 MB (90.94%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.py
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.py
@@ -1,8 +1,6 @@
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
-        res = []
+        res = ['']*len(nums)
         for i in range(len(nums)):
-            curr = nums[i][i]
-            res.append("1" if curr == "0" else "0")
-        
-        return "".join(res)
+            res[i] = '0' if nums[i][i] == '1' else '1'
+        return ''.join(res)
