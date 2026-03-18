6d44ef906e903278e8eee6e236e9ac001111d70b
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2026-03-17 19:49:18 -0700

    Time: 312 ms (64.83%), Space: 66.5 MB (56.55%) - LeetHub

--- 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.py
+++ 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.py
@@ -3,9 +3,8 @@ class Solution:
         M,N = len(grid),len(grid[0])
         A = [[0]*(N+1) for _ in range(M+1)]
         res = 0
-        for i in range(1,M+1):
-            for j in range(1,N+1):
-                A[i][j] = A[i][j-1] + A[i-1][j] - A[i-1][j-1] + grid[i-1][j-1]
-                if A[i][j] <= k:
-                    res += 1
+        for i in range(1, M+1):
+            for j in range(1, N+1):
+                A[i][j] = A[i-1][j] + A[i][j-1] - A[i-1][j-1] + grid[i-1][j-1]
+                res += A[i][j] <= k
         return res
