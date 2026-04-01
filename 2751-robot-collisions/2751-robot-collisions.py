a3d0832e5e9ef317f322faa3f9c7abb14fc8e962
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2026-03-31 22:03:11 -0700

    Time: 91 ms (92.26%), Space: 38.3 MB (79.17%) - LeetHub

--- 2751-robot-collisions/2751-robot-collisions.py
+++ 2751-robot-collisions/2751-robot-collisions.py
@@ -1,28 +1,22 @@
 class Solution:
     def survivedRobotsHealths(self, positions: List[int], healths: List[int], directions: str) -> List[int]:
-        p = sorted(list(range(len(positions))), key=lambda x:positions[x])
+        N = len(positions)
+        P = sorted(list(range(N)), key=lambda x:positions[x])
         st = []
-        i = 0
-        res = []
-        while i < len(p):
-            idx = p[i]
-            if directions[idx] == 'R':
-                st.append([healths[idx], idx])
-                i += 1
-                continue
-            
-            while st and healths[idx] > st[-1][0]:
-                healths[idx] -= 1
-                st.pop()
-            if st:
-                if healths[idx] == st[-1][0]:
-                    st.pop()
-                else:
-                    st[-1][0] -= 1
-            else:
-                res.append([healths[idx], idx])
-            i += 1
-        res.extend(st)
-        return [a[0] for a in sorted(res, key=lambda x:x[1])]
-
-                
+        for i in P:
+            survive = True
+            if directions[i] == 'L':
+                while st and directions[st[-1]] == 'R':
+                    if healths[st[-1]] < healths[i]:
+                        st.pop()
+                        healths[i] -= 1
+                    else: 
+                        if healths[st[-1]] > healths[i]:
+                            healths[st[-1]] -= 1
+                        else:
+                            st.pop()
+                        survive = False
+                        break
+            if survive:
+                st.append(i)
+        return [healths[i] for i in sorted(st)]
