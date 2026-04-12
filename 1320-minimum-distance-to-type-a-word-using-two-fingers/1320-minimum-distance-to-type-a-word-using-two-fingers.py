e4f83503ca39b4fb0b7ae4ed102b10b9d075e5e3
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2026-04-12 00:40:08 -0700

    Time: 59 ms (87.57%), Space: 19.5 MB (78.53%) - LeetHub

--- 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.py
+++ 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.py
@@ -9,17 +9,17 @@ class Solution:
                 j = 0
                 i += 1
         def get_dist(i,j):
-            i,j = i-1,j-1
-            if i < 0 or j < 0: return 0
-            a,b = mp[word[i]]
-            c,d = mp[word[j]]
+            a,b = i//6,i%6
+            c,d = j//6,j%6
             return abs(a-c) + abs(b-d)
         N = len(word)
-        dp = [[inf]*(N+1) for _ in range(N+1)]
-        for k in range(N+1):
+        dp = [[inf]*26 for _ in range(N+1)]
+        for k in range(26):
             dp[0][k] = dp[1][k] = 0
-        for i in range(1,N+1):
-            for k in range(i-1):
-                dp[i][k] = min(dp[i][k], dp[i-1][k] + get_dist(i-1,i))
-                dp[i][i-1] = min(dp[i][i-1], dp[i-1][k] + get_dist(k,i))
-        return min(dp[i])
+        for i in range(1, N+1):
+            c = ord(word[i-1]) - 65
+            p = ord(word[i-2]) - 65
+            for k in range(26):
+                dp[i][k] = min(dp[i][k], dp[i-1][k] + get_dist(p, c))
+                dp[i][p] = min(dp[i][p], dp[i-1][k] + get_dist(k, c))
+        return min(dp[-1])
