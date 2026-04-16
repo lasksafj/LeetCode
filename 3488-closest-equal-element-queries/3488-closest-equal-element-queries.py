e437ca6326428327c226d4dcae282a0166574299
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2026-04-15 23:47:12 -0700

    Time: 207 ms (99.22%), Space: 54.7 MB (73.44%) - LeetHub

--- 3488-closest-equal-element-queries/3488-closest-equal-element-queries.py
+++ 3488-closest-equal-element-queries/3488-closest-equal-element-queries.py
@@ -1,26 +1,20 @@
 class Solution:
     def solveQueries(self, nums: List[int], queries: List[int]) -> List[int]:
         N = len(nums)
-        mp = defaultdict(lambda: SortedList())
+        mp = defaultdict(list)
         for i,n in enumerate(nums):
-            mp[n].add(i)
-        res = []
-        for i in queries:
-            n = nums[i]
-            A = mp[n]
+            mp[n].append(i)
+        res = [-1]*N
+        for n,A in mp.items():
             if len(A) == 1:
-                res.append(-1)
                 continue
-            A.remove(i)
-            j = A.bisect_right(i)
-            if j:
-                a = i - A[j-1]
-            else:
-                a = N - (A[-1]-i)
-            if j < len(A):
-                b = A[j] - i
-            else:
-                b = N - (i-A[0])
-            res.append(min(a,b))
-            A.add(i)
-        return res
+            for i,k in enumerate(A):
+                if i:
+                    res[k] = k-A[i-1]
+                else:
+                    res[k] = N-(A[i-1]-k)
+                if i+1 < len(A):
+                    res[k] = min(res[k], A[i+1]-k)
+                else:
+                    res[k] = min(res[k], N-(k-A[0]))
+        return [res[q] for q in queries]
