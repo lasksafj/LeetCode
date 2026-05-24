ca6fc73eee4ac889da3a089d0923daa763a0877d
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2026-05-23 17:13:32 -0700

    Time: 271 ms (23.72%), Space: 56.4 MB (5.14%) - LeetHub

--- 1340-jump-game-v/1340-jump-game-v.py
+++ 1340-jump-game-v/1340-jump-game-v.py
@@ -1,24 +1,18 @@
 class Solution:
     def maxJumps(self, arr: List[int], d: int) -> int:
         N = len(arr)
+        adj = defaultdict(list)
+        for i in range(N):
+            for j in range(i-1, max(0,i-d)-1, -1):
+                if arr[j] >= arr[i]: break
+                adj[i].append(j)
+            for j in range(i+1, min(N-1,i+d)+1):
+                if arr[j] >= arr[i]: break
+                adj[i].append(j)
         @cache
         def dfs(i):
-            # print(i)
-            res = 0
-            for j in range(i+1, min(N,i+d+1)):
-                if arr[j] < arr[i]:
-                    res = max(res, dfs(j))
-                else:
-                    break
-            for j in range(i-1, max(-1,i-d-1,-1), -1):
-                if arr[j] < arr[i]:
-                    res = max(res, dfs(j))
-                else:
-                    break
-            return res + 1
-        res = 0
-        # print(dfs(10))
-        for i in range(N):
-            res = max(res, dfs(i))
-        return res
-                    
+            res = 1
+            for j in adj[i]:
+                res = max(res, dfs(j)+1)
+            return res
+        return max(dfs(i) for i in range(N))
