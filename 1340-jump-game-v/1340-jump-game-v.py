ab43d5ecf8c930b1e82aa078a4499f6d429b1e30
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2026-05-23 17:20:21 -0700

    Time: 55 ms (96.29%), Space: 22.7 MB (42.86%) - LeetHub

--- 1340-jump-game-v/1340-jump-game-v.py
+++ 1340-jump-game-v/1340-jump-game-v.py
@@ -2,13 +2,27 @@ class Solution:
     def maxJumps(self, arr: List[int], d: int) -> int:
         N = len(arr)
         adj = defaultdict(list)
+        # for i in range(N):
+        #     for j in range(i-1, max(0,i-d)-1, -1):
+        #         if arr[j] >= arr[i]: break
+        #         adj[i].append(j)
+        #     for j in range(i+1, min(N-1,i+d)+1):
+        #         if arr[j] >= arr[i]: break
+        #         adj[i].append(j)
+        st = []
         for i in range(N):
-            for j in range(i-1, max(0,i-d)-1, -1):
-                if arr[j] >= arr[i]: break
-                adj[i].append(j)
-            for j in range(i+1, min(N-1,i+d)+1):
-                if arr[j] >= arr[i]: break
-                adj[i].append(j)
+            while st and arr[st[-1]] < arr[i]:
+                j = st.pop()
+                if i-j <= d:
+                    adj[i].append(j)
+            st.append(i)
+        st = []
+        for i in range(N-1,-1,-1):
+            while st and arr[st[-1]] < arr[i]:
+                j = st.pop()
+                if j-i <= d:
+                    adj[i].append(j)
+            st.append(i)
         @cache
         def dfs(i):
             res = 1
