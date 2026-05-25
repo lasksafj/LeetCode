d809e679507f82c49a9d5e41d005054a4b59cd97
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2026-05-25 00:42:17 -0700

    Time: 118 ms (95.65%), Space: 35.4 MB (93.48%) - LeetHub

--- 2297-jump-game-viii/2297-jump-game-viii.py
+++ 2297-jump-game-viii/2297-jump-game-viii.py
@@ -1,26 +1,17 @@
 class Solution:
     def minCost(self, nums: List[int], costs: List[int]) -> int:
         N = len(nums)
-        adj = defaultdict(list)
-        st = []
+        dp = [inf]*N
+        dp[0] = 0
+        st1 = []
+        st2 = []
         for i in range(N):
-            while st and nums[st[-1]] <= nums[i]:
-                j = st.pop()
-                adj[j].append(i)
-            st.append(i)
-        st = []
-        for i in range(N):
-            while st and nums[st[-1]] > nums[i]:
-                j = st.pop()
-                adj[j].append(i)
-            st.append(i)
-        pq = [[0,0]]
-        dist = [inf]*N
-        dist[0] = 0
-        while pq:
-            d,i = heappop(pq)
-            for j in adj[i]:
-                if dist[j] > d+costs[j]:
-                    heappush(pq, [d+costs[j], j])
-                    dist[j] = d+costs[j]
-        return dist[-1]
+            while st1 and nums[st1[-1]] <= nums[i]:
+                j = st1.pop()
+                dp[i] = min(dp[i], dp[j] + costs[i])
+            st1.append(i)
+            while st2 and nums[st2[-1]] > nums[i]:
+                j = st2.pop()
+                dp[i] = min(dp[i], dp[j] + costs[i])
+            st2.append(i)
+        return dp[-1]
