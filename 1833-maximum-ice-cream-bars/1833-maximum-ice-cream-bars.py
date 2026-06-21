8e2323164589bc1c3adb8b495d0dd95c4150f04c
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2026-06-20 17:24:23 -0700

    Time: 66 ms (86.67%), Space: 30.9 MB (90.23%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
@@ -1,10 +1,13 @@
 class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
+        ma = max(costs)
+        A = [0]*(ma+1)
+        for c in costs:
+            A[c] += 1
         res = 0
-        for c in sorted(costs):
-            coins -= c
-            if coins >= 0:
-                res += 1
-            else:
-                return res
+        for c,v in enumerate(A):
+            if v == 0: continue
+            d = min(v, coins//c)
+            res += d
+            coins -= d*c
         return res
