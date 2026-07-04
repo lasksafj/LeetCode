e00b3304d55a937137f5960589b1beacca17297a
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2026-07-03 18:05:12 -0700

    Time: 174 ms (51.89%), Space: 52.7 MB (90.06%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -1,17 +1,27 @@
 class Solution:
     def minScore(self, n: int, roads: List[List[int]]) -> int:
-        adj = defaultdict(list)
-        for a,b,c in roads:
-            adj[a].append([b,c])
-            adj[b].append([a,c])
-        vis = set()
-        q = deque([1])
-        res = 100001
-        while q:
-            c = q.popleft()
-            for ne,d in adj[c]:
+        f = list(range(n+1))
+        r = [0]*(n+1)
+        s = [inf]*(n+1)
+        def root(x):
+            if x == f[x]:
+                return x
+            return root(f[x])
+        def union(a,b):
+            a,b = root(a),root(b)
+            if a == b:
+                return
+            if r[a] < r[b]:
+                a,b = b,a
+            f[b] = a
+            if r[a] == r[b]:
+                r[a] += 1
+        for a,b,d in roads:
+            union(a,b)
+        if root(1) != root(n): return -1
+        res = inf
+        for a,b,d in roads:
+            if root(a) == root(1):
                 res = min(res, d)
-                if ne not in vis:
-                    q.append(ne)
-                    vis.add(ne)
+            
         return res
