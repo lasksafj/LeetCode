72b2416d10bbd444d3aeb8fa37ac311b324d95ba
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2026-08-11 00:04:56 -0700

    Time: 506 ms (73.35%), Space: 20.1 MB (55.09%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.py
+++ 1510-stone-game-iv/1510-stone-game-iv.py
@@ -1,14 +1,12 @@
 class Solution:
     def winnerSquareGame(self, n: int) -> bool:
-        dp = [[0]*2 for _ in range(n+1)]
-        dp[0][0] = 1
+        dp = [0]*(n+1)
         for i in range(n+1):
-            for t in [0,1]:
-                mi = 1
-                for a in range(1, int(sqrt(i))+1):
-                    a *= a
-                    if dp[i-a][t^1] == 0:
-                        mi = 0
-                        break
-                dp[i][t] = mi^1
-        return True if dp[n][1] else False
+            mi = 1
+            for a in range(1, int(sqrt(i))+1):
+                a *= a
+                if dp[i-a] == 0:
+                    mi = 0
+                    break
+            dp[i] = mi^1
+        return True if dp[n] else False
