32aa7d537b9bc5177d5388d58ae0a179a2ea6c70
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2026-08-11 00:01:39 -0700

    Time: 1560 ms (20.36%), Space: 27.1 MB (38.92%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.py
+++ 1510-stone-game-iv/1510-stone-game-iv.py
@@ -8,8 +8,8 @@ class Solution:
         for i in range(n+1):
             for t in [0,1]:
                 mi = 1
-                for a in sq:
-                    if i-a < 0: break
+                for a in range(1, int(sqrt(i))+1):
+                    a *= a
                     if dp[i-a][t^1] == 0:
                         mi = 0
                         break
