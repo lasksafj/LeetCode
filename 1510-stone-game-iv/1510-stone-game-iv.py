80db7a81fc86e04a2118cc6bb620da6528860462
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2026-08-11 00:13:12 -0700

    Time: 90 ms (97.01%), Space: 23.9 MB (43.11%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.py
+++ 1510-stone-game-iv/1510-stone-game-iv.py
@@ -1,12 +1,13 @@
 class Solution:
     def winnerSquareGame(self, n: int) -> bool:
-        dp = [0]*(n+1)
-        for i in range(n+1):
-            mi = 1
-            for a in range(1, int(sqrt(i))+1):
-                a *= a
-                if dp[i-a] == 0:
-                    mi = 0
-                    break
-            dp[i] = mi^1
-        return True if dp[n] else False
+        # state with remaining stone i after end of current player turn
+        @cache
+        def dfs(i):
+            # rem stone = 0 after current player move => current player lose
+            if i == 0: return False
+
+            for k in range(int(sqrt(i)), 0, -1):
+                if not dfs(i-k*k):
+                    return True
+            return False
+        return dfs(n)
