2c4faa556bcbda943b9ea583754c66eed60583ba
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2026-08-28 21:11:33 -0700

    Time: 230 ms (85.82%), Space: 95.2 MB (21.64%) - LeetHub

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.py
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.py
@@ -1,24 +1,16 @@
 class Solution:
     def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
-        N = len(nums)
-        A = sorted([n,i] for i,n in enumerate(nums))
-        grps = []
-        i = 0
-        while i < N:
-            j = i+1
-            grp = [A[i]]
-            while j < N and A[j][0] - A[j-1][0] <= limit:
-                grp.append(A[j])
-                j += 1            
-            grps.append(grp)
-            i = j
-        res = [0]*N
-        for grp in grps:
-            I = []
-            for _,i in grp:
-                I.append(i)
-            j = 0
-            for i in sorted(I):
-                res[i] = grp[j][0]
-                j += 1
+        A = sorted(nums)
+        p = -inf
+        B = []
+        mp = {}
+        for a in A:
+            if a-limit > p:
+                B.append(deque())
+            B[-1].append(a)
+            mp[a] = len(B)-1
+            p = a
+        res = []
+        for n in nums:
+            res.append(B[mp[n]].popleft())
         return res
