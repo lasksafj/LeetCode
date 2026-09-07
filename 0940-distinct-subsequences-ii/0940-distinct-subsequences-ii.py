4afe26d6a2e34f41f4494b327a1f6071fa015031
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2026-09-06 22:18:35 -0700

    Time: 15 ms (30.21%), Space: 19.5 MB (14.58%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.py
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.py
@@ -3,10 +3,8 @@ class Solution:
         s = '#' + s
         n = len(s)
         dp = [0]*n
-        dp2 = [0]*n
         last = {}
         for i in range(1,n):
-            dp[i] = dp[i-1]*2 + 1 - (dp2[last[s[i]]] if s[i] in last else 0)
-            dp2[i] = dp[i-1] + 1 - (dp2[last[s[i]]-1] if s[i] in last else 0)
+            dp[i] = dp[i-1]*2 + 1 - ((dp[last[s[i]]-1] + 1) if s[i] in last else 0)
             last[s[i]] = i
-        return dp[-1]
+        return dp[-1] % (10**9+7)
