49e114b09534d8c1224bf7c1c108ab15746c3196
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2026-09-09 10:28:57 -0700

    Time: 0 ms (100%), Space: 19.2 MB (46.52%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.py
+++ 3870-count-commas-in-range/3870-count-commas-in-range.py
@@ -1,9 +1,10 @@
 class Solution:
     def countCommas(self, n: int) -> int:
-        p = 3
         res = 0
-        while n >= 10**p:
-            n -= 10**p
-            p += 3
-            res += n%(10**p) + 1
+        d = 10**3
+        c = 1
+        while n >= d:
+            res += c * (min(n, d * 10**3) - d) + 1
+            c += 1
+            d *= 10**3
         return res
